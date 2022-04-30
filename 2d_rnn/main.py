abb0da43f0be0246174e4ce35e031ccb48f49a86
Daniel Chin <daniel.chin@nyu.edu>
2022-04-30 09:07:51 -0400

    experiments with configs

---
+++ 2d_rnn/main.py
@@ -0,0 +1,185 @@
+import os
+from collections import namedtuple
+from itertools import count
+import torch
+from PIL import Image
+
+try:
+    from myTorch import LossLogger
+except ImportError as e:
+    module_name = str(e).split('No module named ', 1)[1].strip().strip('"\'')
+    if module_name in (
+        'myTorch', 
+    ):
+        print(f'Missing module {module_name}. Please download at')
+        print(f'https://github.com/Daniel-Chin/Python_Lib')
+        input('Press Enter to quit...')
+    raise e
+
+from vae import VAE
+from rnn import RNN
+from makeDataset import (
+    TRAIN_SET_SIZE, VALIDATE_SET_SIZE, SEQ_LEN, RESOLUTION, 
+)
+from loadDataset import loadDataset, TRAIN_PATH, VALIDATE_PATH
+from train import (
+    oneEpoch, HAS_CUDA, DEVICE, BATCH_SIZE, oneBatch, 
+    RNN_MIN_CONTEXT, 
+)
+
+Config = namedtuple('Config', [
+    'beta', 'vae_loss_coef', 'rnn_loss_coef', 
+])
+
+RAND_INIT_TIMES = 5
+EXPERIMENTS = [
+    Config(.001, 1, 1), 
+]
+
+EXPERIMENTS_PATH = './experiments'
+
+def loadModel():
+    # future: load model from disk
+    vae = VAE()
+    rnn = RNN()
+    if HAS_CUDA:
+        vae = vae.cuda()
+        rnn = rnn.cuda()
+    return vae, rnn
+
+def renderExperimentPath(
+    rand_init_i, config: Config, 
+):
+    return os.path.join(
+        EXPERIMENTS_PATH, f'''{(
+            rand_init_i, *config, 
+        )}'''
+    )
+
+class Trainer:
+    def __init__(
+        self, vae, rnn, optim, train_set, validate_set, 
+        rand_init_i, config: Config, 
+    ) -> None:
+        self.vae = vae
+        self.rnn = rnn
+        self.optim = optim
+        self.train_set = train_set
+        self.validate_set = validate_set
+
+        self.config = config
+        self.experiment_path = renderExperimentPath(
+            rand_init_i, config, 
+        )
+        os.makedirs(self.experiment_path, exist_ok=True)
+
+        self.entered = False
+        self.prev_cwd = None
+
+        with self:
+            self.lossLogger = LossLogger('losses.log')
+        self.lossLogger.clearFile()
+    
+    def __enter__(self):
+        assert not self.entered
+        self.prev_cwd = os.getcwd()
+        os.chdir(self.experiment_path)
+        self.entered = True
+    
+    def __exit__(self, *_):
+        os.chdir(self.prev_cwd)
+        self.entered = False
+        return False
+    
+    def oneEpoch(self, epoch):
+        with self:
+            oneEpoch(
+                epoch, self.vae, self.rnn, self.optim, 
+                self.train_set, self.validate_set, 
+                self.lossLogger, *self.config, 
+            )
+            if epoch % 1 == 0:
+                torch.save(
+                    self.vae.state_dict(), f'{epoch}_vae.pt', 
+                )
+                torch.save(
+                    self.rnn.state_dict(), f'{epoch}_rnn.pt', 
+                )
+
+def main():
+    train_set    = loadDataset(   TRAIN_PATH, DEVICE)
+    validate_set = loadDataset(VALIDATE_PATH, DEVICE)
+    assert TRAIN_SET_SIZE % BATCH_SIZE == 0
+    assert VALIDATE_SET_SIZE == BATCH_SIZE
+    trainers = []
+    for config in EXPERIMENTS:
+        for rand_init_i in range(RAND_INIT_TIMES):
+            vae, rnn = loadModel()
+            optim = torch.optim.Adam(
+                [
+                    *vae.parameters(), *rnn.parameters(), 
+                ], lr=.001, 
+            )
+            trainers.append(Trainer(
+                vae, rnn, optim, train_set, validate_set, 
+                rand_init_i, config, 
+            ))
+    for epoch in count():
+        for trainer in trainers:
+            trainer: Trainer
+            print(trainer.config)
+            trainer.oneEpoch(epoch)
+        if epoch % 20 == 0:
+            print('making GIFs.')
+            with torch.no_grad():
+                for trainer in trainers:
+                    with trainer:
+                        evalGIFs(
+                            trainer.vae.cpu(), 
+                            trainer.rnn.cpu(), 
+                            validate_set[:24, :, :, :, :], 
+                        )
+            print('GIFs made.')
+
+def torch2PIL(torchImg: torch.Tensor):
+    return Image.fromarray(torchImg.numpy() * 255).convert('L')
+
+def evalGIFs(vae: VAE, rnn: RNN, dataset: torch.Tensor):
+    n_datapoints = dataset.shape[0]
+    vae.eval()
+    rnn.eval()
+    predictions, reconstructions = oneBatch(
+        vae, rnn, dataset, 0, 0, 0, 
+        True, batch_size=n_datapoints, 
+    )
+    frames = []
+    for t in range(SEQ_LEN):
+        frame = Image.new('L', (
+            RESOLUTION * n_datapoints, RESOLUTION * 3, 
+        ))
+        frames.append(frame)
+        for i in range(n_datapoints):
+            frame.paste(
+                torch2PIL(dataset[i, t, 0, :, :]), 
+                (i * RESOLUTION, 0 * RESOLUTION), 
+            )
+            frame.paste(
+                torch2PIL(reconstructions[i, t, 0, :, :]), 
+                (i * RESOLUTION, 1 * RESOLUTION), 
+            )
+            if t >= RNN_MIN_CONTEXT:
+                frame.paste(
+                    torch2PIL(predictions[
+                        i, t - RNN_MIN_CONTEXT, 0, :, :, 
+                    ]), 
+                    (i * RESOLUTION, 2 * RESOLUTION), 
+                )
+    filename = 'pred.gif'
+    frames[0].save(
+        filename, 
+        save_all=True, append_images=frames[1:], 
+        duration=300, loop=0, 
+    )
+    print(f'Saved `{filename}`.')
+
+main()
--- 2d_rnn/train.py
+++ 2d_rnn/train.py
@@ -1,6 +1,5 @@
 import torch
 import torch.nn.functional as F
-from PIL import Image
 
 try:
     from myTorch import LossLogger
@@ -13,20 +12,14 @@ except ImportError as e:
         print(f'https://github.com/Daniel-Chin/Python_Lib')
         input('Press Enter to quit...')
     raise e
-from loadDataset import loadDataset, TRAIN_PATH, VALIDATE_PATH
 from makeDataset import (
-    RESOLUTION, SEQ_LEN, TRAIN_SET_SIZE, VALIDATE_SET_SIZE, 
+    RESOLUTION, SEQ_LEN, TRAIN_SET_SIZE, 
 )
 from vae import LATENT_DIM, VAE
 from rnn import RNN
 
-N_EPOCHS = 1000
 BATCH_SIZE = 32
-VAE_LOSS_COEF = 1
-RNN_LOSS_COEF = 1
 RNN_MIN_CONTEXT = 3
-
-CHECKPOINTS_PATH = './checkpoints'
 
 HAS_CUDA = torch.cuda.is_available()
 CUDA = torch.device("cuda:0")
@@ -38,93 +31,65 @@ else:
     DEVICE = CPU
     print("We DON'T have CUDA.")
 
-def loadModel():
-    # future: load model from disk
-    vae = VAE()
-    rnn = RNN()
-    if HAS_CUDA:
-        vae = vae.cuda()
-        rnn = rnn.cuda()
-    return vae, rnn
-
-def train(rand_init_i=0, beta=0.0005):
-    # this_checkpoints_path = path.join(
-    #     CHECKPOINTS_PATH, f'{beta}_{rand_init_i}', 
-    # )
-    # os.makedirs(this_checkpoints_path, exist_ok=True)
-    vae, rnn = loadModel()
-    optim = torch.optim.Adam(
-        [*vae.parameters(), *rnn.parameters()], lr=.001, 
-    )
-    train_set    = loadDataset(   TRAIN_PATH, DEVICE)
-    validate_set = loadDataset(VALIDATE_PATH, DEVICE)
-    assert TRAIN_SET_SIZE % BATCH_SIZE == 0
-    assert VALIDATE_SET_SIZE == BATCH_SIZE
+def oneEpoch(
+    epoch, 
+    vae: VAE, rnn: RNN, optim, train_set, validate_set, 
+    lossLogger: LossLogger, 
+    beta=0.001, vae_loss_coef=1, rnn_loss_coef=1, 
+):
     n_batches = TRAIN_SET_SIZE // BATCH_SIZE
-    lossLogger = LossLogger('losses.log')
-    lossLogger.clearFile()
-    try:
-        for epoch in range(N_EPOCHS):
-            vae.train()
-            rnn.train()
-            epoch_recon__loss = 0
-            epoch_kld____loss = 0
-            epoch_z_pred_loss = 0
-            for batch_pos in range(0, TRAIN_SET_SIZE, BATCH_SIZE):
-                batch = train_set[
-                    batch_pos : batch_pos + BATCH_SIZE, 
-                    :, :, :, :, 
-                ]
-                batch_pos = (
-                    batch_pos + BATCH_SIZE
-                ) % TRAIN_SET_SIZE
+    vae.train()
+    rnn.train()
+    epoch_recon__loss = 0
+    epoch_kld____loss = 0
+    epoch_z_pred_loss = 0
+    for batch_pos in range(0, TRAIN_SET_SIZE, BATCH_SIZE):
+        batch = train_set[
+            batch_pos : batch_pos + BATCH_SIZE, 
+            :, :, :, :, 
+        ]
+        batch_pos = (
+            batch_pos + BATCH_SIZE
+        ) % TRAIN_SET_SIZE
 
-                (
-                    total_loss, recon_loss, kld_loss, rnn_loss, 
-                ) = oneBatch(
-                    vae, rnn, batch, beta, 
-                )
-                
-                optim.zero_grad()
-                total_loss.backward()
-                optim.step()
-
-                epoch_recon__loss += recon_loss / n_batches
-                epoch_kld____loss +=   kld_loss / n_batches
-                epoch_z_pred_loss +=   rnn_loss / n_batches
-
-            vae.eval()
-            rnn.eval()
-            with torch.no_grad():
-                (
-                    _, validate_recon__loss, 
-                    validate_kld____loss, validate_z_pred_loss, 
-                ) = oneBatch(
-                    vae, rnn, validate_set, beta, 
-                )
-            lossLogger.eat(
-                epoch, 
-                train____recon__loss=epoch_recon__loss, 
-                validate_recon__loss=validate_recon__loss, 
-                train____kld____loss=epoch_kld____loss, 
-                validate_kld____loss=validate_kld____loss, 
-                train____z_pred_loss=epoch_z_pred_loss, 
-                validate_z_pred_loss=validate_z_pred_loss, 
-            )
-            # if epoch % 1 == 0:
-            #     torch.save(vae.state_dict(), path.join(
-            #         this_checkpoints_path, f'{epoch}.pt', 
-            #     ))
-    except KeyboardInterrupt:
-        print('Received ^C.')
-    with torch.no_grad():
-        evalGIFs(
-            vae.cpu(), rnn.cpu(), 
-            validate_set[:12, :, :, :, :], 
+        (
+            total_loss, recon_loss, kld_loss, rnn_loss, 
+        ) = oneBatch(
+            vae, rnn, batch, beta, 
+            vae_loss_coef, rnn_loss_coef, 
         )
+        
+        optim.zero_grad()
+        total_loss.backward()
+        optim.step()
+
+        epoch_recon__loss += recon_loss / n_batches
+        epoch_kld____loss +=   kld_loss / n_batches
+        epoch_z_pred_loss +=   rnn_loss / n_batches
+
+    vae.eval()
+    rnn.eval()
+    with torch.no_grad():
+        (
+            _, validate_recon__loss, 
+            validate_kld____loss, validate_z_pred_loss, 
+        ) = oneBatch(
+            vae, rnn, validate_set, beta, 
+            vae_loss_coef, rnn_loss_coef, 
+        )
+    lossLogger.eat(
+        epoch, 
+        train____recon__loss=epoch_recon__loss, 
+        validate_recon__loss=validate_recon__loss, 
+        train____kld____loss=epoch_kld____loss, 
+        validate_kld____loss=validate_kld____loss, 
+        train____z_pred_loss=epoch_z_pred_loss, 
+        validate_z_pred_loss=validate_z_pred_loss, 
+    )
 
 def oneBatch(
     vae: VAE, rnn: RNN, batch: torch.Tensor, beta, 
+    vae_loss_coef, rnn_loss_coef, 
     visualize=False, batch_size = BATCH_SIZE, 
 ):
     flat_batch = batch.view(
@@ -149,60 +114,22 @@ def oneBatch(
         # that probably gonna break autograd
         # wait what it didn't
         rnn.stepTime(z_hat, t)
-    rnn_loss = F.mse_loss(z_hat, z[:, RNN_MIN_CONTEXT:, :])
+    predictions = vae.decode(z_hat.view(-1, LATENT_DIM)).view(
+        batch_size, SEQ_LEN - RNN_MIN_CONTEXT, 
+        1, RESOLUTION, RESOLUTION, 
+    )
+    rnn_loss = F.mse_loss(predictions, batch[
+        :, RNN_MIN_CONTEXT:, :, :, :, 
+    ])
     
     total_loss = (
-        vae_loss * VAE_LOSS_COEF + 
-        rnn_loss * RNN_LOSS_COEF
+        vae_loss * vae_loss_coef + 
+        rnn_loss * rnn_loss_coef
     )
 
     if visualize:
-        return z_hat, reconstructions.view(
+        return predictions, reconstructions.view(
             batch_size, SEQ_LEN, 1, RESOLUTION, RESOLUTION, 
         )
     else:
         return total_loss, recon_loss, kld_loss, rnn_loss
-
-def torch2PIL(torchImg: torch.Tensor):
-    return Image.fromarray(torchImg.numpy() * 255).convert('L')
-
-def evalGIFs(vae: VAE, rnn: RNN, dataset: torch.Tensor):
-    n_datapoints = dataset.shape[0]
-    z_hat, reconstructions = oneBatch(
-        vae, rnn, dataset, 0, True, batch_size=n_datapoints, 
-    )
-    predictions = vae.decode(
-        z_hat.view(-1, LATENT_DIM)
-    ).view(n_datapoints, -1, 1, RESOLUTION, RESOLUTION)
-    frames = []
-    for t in range(SEQ_LEN):
-        frame = Image.new('L', (
-            RESOLUTION * n_datapoints, RESOLUTION * 3, 
-        ))
-        frames.append(frame)
-        for i in range(n_datapoints):
-            frame.paste(
-                torch2PIL(dataset[i, t, 0, :, :]), 
-                (i * RESOLUTION, 0 * RESOLUTION), 
-            )
-            frame.paste(
-                torch2PIL(reconstructions[i, t, 0, :, :]), 
-                (i * RESOLUTION, 1 * RESOLUTION), 
-            )
-            if t >= RNN_MIN_CONTEXT:
-                frame.paste(
-                    torch2PIL(predictions[
-                        i, t - RNN_MIN_CONTEXT, 0, :, :, 
-                    ]), 
-                    (i * RESOLUTION, 2 * RESOLUTION), 
-                )
-    filename = 'pred.gif'
-    frames[0].save(
-        filename, 
-        save_all=True, append_images=frames[1:], 
-        duration=300, loop=0, 
-    )
-    print(f'Saved `{filename}`.')
-
-if __name__ == '__main__':
-    train()

81ded539156df88b6e26d4dbd4157d92e344a4bf
Daniel Chin <daniel.chin@nyu.edu>
2022-05-06 09:25:30 -0400

    a

--- bounce/experiments.py
+++ bounce/experiments.py
@@ -1,50 +1,26 @@
 from train import Config
 from betaSched import Constant, WarmUp, Osc
 
-RAND_INIT_TIMES = 4
-# EXPERIMENTS = [
-#     ('AE', Config(
-#         Constant(0), 1, 0, do_symmetry=False, variational_rnn=False, 
-#         rnn_width=32, deep_spread=False, 
-#     )), 
-#     ('AE+RNN+symm', Config(
-#         Constant(0), 1, 1, do_symmetry=True, variational_rnn=False, 
-#         rnn_width=32, deep_spread=False, 
-#     )), 
-#     ('VAE+RNN+symm', Config(
-#         Constant(0.0005), 1, 1, do_symmetry=True, variational_rnn=False, 
-#         rnn_width=32, deep_spread=False, 
-#     )), 
-#     ('VAE+VRNN+symm', Config(
-#         Constant(0.0005), 1, 1, do_symmetry=True, variational_rnn=True, 
-#         rnn_width=32, deep_spread=False, 
-#     )), 
-# ]
-
+RAND_INIT_TIMES = 2
 EXPERIMENTS = [
-    ('VAE', Config(
-        Constant(1e-8), 1, 0, do_symmetry=False, 
+    ('VAE+RNN', Config(
+        Constant(1e-5), 1, 1, do_symmetry=False, 
         variational_rnn=False, rnn_width=32, 
         deep_spread=False, vae_channels=[16, 32, 64], 
     )), 
-    ('VAE', Config(
-        Constant(1e-7), 1, 0, do_symmetry=False, 
+    ('AE+RNN+symm', Config(
+        Constant(0), 1, 1, do_symmetry=True, 
         variational_rnn=False, rnn_width=32, 
         deep_spread=False, vae_channels=[16, 32, 64], 
     )), 
-    ('VAE', Config(
-        Constant(1e-6), 1, 0, do_symmetry=False, 
+    ('VAE+RNN+symm', Config(
+        Constant(1e-5), 1, 1, do_symmetry=True, 
         variational_rnn=False, rnn_width=32, 
         deep_spread=False, vae_channels=[16, 32, 64], 
     )), 
-    ('VAE', Config(
-        Constant(1e-5), 1, 0, do_symmetry=False, 
-        variational_rnn=False, rnn_width=32, 
-        deep_spread=False, vae_channels=[16, 32, 64], 
-    )), 
-    ('VAE', Config(
-        Constant(1e-4), 1, 0, do_symmetry=False, 
-        variational_rnn=False, rnn_width=32, 
+    ('VAE+VRNN+symm', Config(
+        Constant(1e-5), 1, 1, do_symmetry=True, 
+        variational_rnn=True, rnn_width=32, 
         deep_spread=False, vae_channels=[16, 32, 64], 
     )), 
 ]

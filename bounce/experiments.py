0c9027650f38cd1154dfbbfaf8886008173ada78
Daniel Chin <daniel.chin@nyu.edu>
2022-05-05 23:18:20 -0400

    beta

--- bounce/experiments.py
+++ bounce/experiments.py
@@ -1,7 +1,7 @@
 from train import Config
 from betaSched import Constant, WarmUp, Osc
 
-RAND_INIT_TIMES = 8
+RAND_INIT_TIMES = 4
 # EXPERIMENTS = [
 #     ('AE', Config(
 #         Constant(0), 1, 0, do_symmetry=False, variational_rnn=False, 
@@ -23,12 +23,27 @@ RAND_INIT_TIMES = 8
 
 EXPERIMENTS = [
     ('VAE', Config(
-        Constant(1e-9), 1, 0, do_symmetry=False, 
+        Constant(1e-8), 1, 0, do_symmetry=False, 
         variational_rnn=False, rnn_width=32, 
-        deep_spread=False, vae_channels=[8, 16, 16, 16], 
+        deep_spread=False, vae_channels=[16, 32, 64], 
     )), 
     ('VAE', Config(
-        Constant(1e-9), 1, 0, do_symmetry=False, 
+        Constant(1e-7), 1, 0, do_symmetry=False, 
+        variational_rnn=False, rnn_width=32, 
+        deep_spread=False, vae_channels=[16, 32, 64], 
+    )), 
+    ('VAE', Config(
+        Constant(1e-6), 1, 0, do_symmetry=False, 
+        variational_rnn=False, rnn_width=32, 
+        deep_spread=False, vae_channels=[16, 32, 64], 
+    )), 
+    ('VAE', Config(
+        Constant(1e-5), 1, 0, do_symmetry=False, 
+        variational_rnn=False, rnn_width=32, 
+        deep_spread=False, vae_channels=[16, 32, 64], 
+    )), 
+    ('VAE', Config(
+        Constant(1e-4), 1, 0, do_symmetry=False, 
         variational_rnn=False, rnn_width=32, 
         deep_spread=False, vae_channels=[16, 32, 64], 
     )), 

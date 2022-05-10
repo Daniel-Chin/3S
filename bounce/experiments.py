daaddc17b76203034f22c1851d3f46204ac56c58
Daniel Chin <daniel.chin@nyu.edu>
2022-05-10 12:04:29 -0400

    exp

--- bounce/experiments.py
+++ bounce/experiments.py
@@ -1,26 +1,21 @@
 from train import Config
 from betaSched import Constant, WarmUp, Osc
 
-RAND_INIT_TIMES = 5
+RAND_INIT_TIMES = 4
 EXPERIMENTS = [
-    ('VAE+RNN', Config(
-        Constant(1e-5), 1, 1, do_symmetry=False, 
-        variational_rnn=False, rnn_width=32, 
-        deep_spread=False, vae_channels=[16, 32, 64], 
-    )), 
-    ('AE+RNN+symm', Config(
-        Constant(0), 1, 1, do_symmetry=True, 
-        variational_rnn=False, rnn_width=32, 
-        deep_spread=False, vae_channels=[16, 32, 64], 
-    )), 
-    ('VAE+RNN+symm', Config(
-        Constant(1e-5), 1, 1, do_symmetry=True, 
-        variational_rnn=False, rnn_width=32, 
-        deep_spread=False, vae_channels=[16, 32, 64], 
-    )), 
-    ('VAE+VRNN+symm', Config(
+    ('nothing', Config(
         Constant(1e-5), 1, 1, do_symmetry=True, 
         variational_rnn=True, rnn_width=32, 
         deep_spread=False, vae_channels=[16, 32, 64], 
+    )), 
+    ('deep spread', Config(
+        Constant(1e-5), 1, 1, do_symmetry=True, 
+        variational_rnn=True, rnn_width=32, 
+        deep_spread=True, vae_channels=[16, 32, 64], 
+    )), 
+    ('deep spread + strong vae', Config(
+        Constant(1e-5), 1, 1, do_symmetry=True, 
+        variational_rnn=True, rnn_width=32, 
+        deep_spread=True, vae_channels=[64, 128, 256], 
     )), 
 ]

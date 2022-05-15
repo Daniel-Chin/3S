efbc6e241360a4a91bb561aff239281c80d8bcbc
Daniel Chin <daniel.chin@nyu.edu>
2022-05-15 12:12:14 -0400

    exp

--- bounce/experiments.py
+++ bounce/experiments.py
@@ -29,38 +29,17 @@ RAND_INIT_TIMES = 2
 #     )), 
 # ]
 
-# EXPERIMENTS = [
-#     ('3', Config(
-#         Constant(1e-5), 1, 1, do_symmetry=True, 
-#         variational_rnn=True, rnn_width=32, 
-#         deep_spread=False, vae_channels=[16, 32, 64], 
-#         vvrnn=False, vvrnn_static=-20, rnn_min_context=3, 
-#     )), 
-#     ('4', Config(
-#         Constant(1e-5), 1, 1, do_symmetry=True, 
-#         variational_rnn=True, rnn_width=32, 
-#         deep_spread=False, vae_channels=[16, 32, 64], 
-#         vvrnn=False, vvrnn_static=-20, rnn_min_context=4, 
-#     )), 
-#     ('5', Config(
-#         Constant(1e-5), 1, 1, do_symmetry=True, 
-#         variational_rnn=True, rnn_width=32, 
-#         deep_spread=False, vae_channels=[16, 32, 64], 
-#         vvrnn=False, vvrnn_static=-20, rnn_min_context=5, 
-#     )), 
-# ]
-
 EXPERIMENTS = [
-    ('rnn', Config(
-        Constant(1e-5), 1, 1, do_symmetry=True, 
-        variational_rnn=False, rnn_width=32, 
-        deep_spread=False, vae_channels=[16, 32, 64], 
-        vvrnn=False, vvrnn_static=-20, rnn_min_context=4, 
-    )), 
-    ('vrnn', Config(
+    ('3', Config(
         Constant(1e-5), 1, 1, do_symmetry=True, 
         variational_rnn=True, rnn_width=32, 
         deep_spread=False, vae_channels=[16, 32, 64], 
-        vvrnn=False, vvrnn_static=-20, rnn_min_context=4, 
+        vvrnn=False, vvrnn_static=-20, rnn_min_context=3, 
+    )), 
+    ('5', Config(
+        Constant(1e-5), 1, 1, do_symmetry=True, 
+        variational_rnn=True, rnn_width=32, 
+        deep_spread=False, vae_channels=[16, 32, 64], 
+        vvrnn=False, vvrnn_static=-20, rnn_min_context=5, 
     )), 
 ]

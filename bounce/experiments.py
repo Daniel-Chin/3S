c5ea06b8a26dd5c866e3f8babeacfeba9a34847a
Daniel Chin <daniel.chin@nyu.edu>
2022-05-16 09:58:32 -0400

    exp

--- bounce/experiments.py
+++ bounce/experiments.py
@@ -1,7 +1,7 @@
 from train import Config
 from betaSched import Constant, WarmUp, Osc
 
-RAND_INIT_TIMES = 2
+RAND_INIT_TIMES = 3
 # EXPERIMENTS = [
 #     ('-10', Config(
 #         Constant(1e-5), 1, 1, do_symmetry=True, 
@@ -51,10 +51,10 @@ EXPERIMENTS = [
         deep_spread=False, vae_channels=[16, 32, 64], 
         vvrnn=False, vvrnn_static=-20, rnn_min_context=5, 
     )), 
-    ('1:1', Config(
-        Constant(1e-5), 1, 1, do_symmetry=True, 
-        variational_rnn=True, rnn_width=32, 
-        deep_spread=False, vae_channels=[16, 32, 64], 
-        vvrnn=False, vvrnn_static=-20, rnn_min_context=5, 
-    )), 
+    # ('1:1', Config(
+    #     Constant(1e-5), 1, 1, do_symmetry=True, 
+    #     variational_rnn=True, rnn_width=32, 
+    #     deep_spread=False, vae_channels=[16, 32, 64], 
+    #     vvrnn=False, vvrnn_static=-20, rnn_min_context=5, 
+    # )), 
 ]

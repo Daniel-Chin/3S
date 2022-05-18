901d391bd245fc110da2bf6699753352f473ec03
Daniel Chin <daniel.chin@nyu.edu>
2022-05-17 22:54:31 -0400

    exp

--- bounce/experiments.py
+++ bounce/experiments.py
@@ -1,7 +1,7 @@
 from train import Config
 from betaSched import Constant, WarmUp, Osc
 
-RAND_INIT_TIMES = 2
+RAND_INIT_TIMES = 3
 # EXPERIMENTS = [
 #     ('-10', Config(
 #         Constant(1e-5), 1, 1, do_symmetry=True, 
@@ -45,20 +45,14 @@ RAND_INIT_TIMES = 2
 # ]
 
 EXPERIMENTS = [
-    ('0.01:1.99', Config(
-        Constant(1e-5), 0.01, 1.99, do_symmetry=True, 
-        variational_rnn=True, rnn_width=32, 
-        deep_spread=False, vae_channels=[16, 32, 64], 
-        vvrnn=False, vvrnn_static=-20, rnn_min_context=5, 
-    )), 
-    ('0.03:1.97', Config(
-        Constant(1e-5), 0.03, 1.97, do_symmetry=True, 
-        variational_rnn=True, rnn_width=32, 
-        deep_spread=False, vae_channels=[16, 32, 64], 
-        vvrnn=False, vvrnn_static=-20, rnn_min_context=5, 
-    )), 
     ('0.1:1.9', Config(
         Constant(1e-5), 0.1, 1.9, do_symmetry=True, 
+        variational_rnn=True, rnn_width=32, 
+        deep_spread=False, vae_channels=[16, 32, 64], 
+        vvrnn=False, vvrnn_static=-20, rnn_min_context=5, 
+    )), 
+    ('1:1', Config(
+        Constant(1e-5), 1, 1, do_symmetry=True, 
         variational_rnn=True, rnn_width=32, 
         deep_spread=False, vae_channels=[16, 32, 64], 
         vvrnn=False, vvrnn_static=-20, rnn_min_context=5, 

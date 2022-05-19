352df3f403cf9e098a2d4d39ee25d4efda821480
Daniel Chin <daniel.chin@nyu.edu>
2022-05-19 09:38:45 -0400

    exp

--- bounce/evalDecoder.py
+++ bounce/evalDecoder.py
@@ -11,7 +11,7 @@ from vae import LATENT_DIM, VAE
 from loadModels import loadModels
 from train import DEVICE
 
-EXP_PATH = 'C:/Users/iGlop/d/symmetry/danRepo/bounce/results/vae_rnn_weight'
+EXP_PATH = 'C:/Users/iGlop/d/symmetry/danRepo/bounce/results/min_context_3'
 
 RADIUS = 2
 TICK_INTERVAL = 0.05
--- bounce/experiments.py
+++ bounce/experiments.py
@@ -4,13 +4,7 @@ from betaSched import Constant, WarmUp, Osc
 RAND_INIT_TIMES = 2
 
 EXPERIMENTS = [
-    ('3', Config(
-        Constant(1e-5), 1, 1, do_symmetry=True, 
-        variational_rnn=True, rnn_width=32, 
-        deep_spread=False, vae_channels=[16, 32, 64], 
-        vvrnn=False, vvrnn_static=-5, rnn_min_context=3, 
-    )), 
-    ('7', Config(
+    ('can we z', Config(
         Constant(1e-5), 1, 1, do_symmetry=True, 
         variational_rnn=True, rnn_width=32, 
         deep_spread=False, vae_channels=[16, 32, 64], 

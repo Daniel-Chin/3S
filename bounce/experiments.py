7738f72091adb50f887dcc5816d63795c8dcbc01
Daniel Chin <daniel.chin@nyu.edu>
2022-05-18 22:45:19 -0400

    exp

--- bounce/experiments.py
+++ bounce/experiments.py
@@ -2,65 +2,18 @@ from train import Config
 from betaSched import Constant, WarmUp, Osc
 
 RAND_INIT_TIMES = 2
-# EXPERIMENTS = [
-#     ('-10', Config(
-#         Constant(1e-5), 1, 1, do_symmetry=True, 
-#         variational_rnn=True, rnn_width=32, 
-#         deep_spread=False, vae_channels=[16, 32, 64], 
-#         vvrnn=False, vvrnn_static=-10, 
-#     )), 
-#     ('-7', Config(
-#         Constant(1e-5), 1, 1, do_symmetry=True, 
-#         variational_rnn=True, rnn_width=32, 
-#         deep_spread=False, vae_channels=[16, 32, 64], 
-#         vvrnn=False, vvrnn_static=-7, 
-#     )), 
-#     ('-4', Config(
-#         Constant(1e-5), 1, 1, do_symmetry=True, 
-#         variational_rnn=True, rnn_width=32, 
-#         deep_spread=False, vae_channels=[16, 32, 64], 
-#         vvrnn=False, vvrnn_static=-4, 
-#     )), 
-#     ('-2', Config(
-#         Constant(1e-5), 1, 1, do_symmetry=True, 
-#         variational_rnn=True, rnn_width=32, 
-#         deep_spread=False, vae_channels=[16, 32, 64], 
-#         vvrnn=False, vvrnn_static=-2, 
-#     )), 
-# ]
-
-# EXPERIMENTS = [
-#     ('3', Config(
-#         Constant(1e-5), 1, 1, do_symmetry=True, 
-#         variational_rnn=True, rnn_width=32, 
-#         deep_spread=False, vae_channels=[16, 32, 64], 
-#         vvrnn=False, vvrnn_static=-20, rnn_min_context=3, 
-#     )), 
-#     ('5', Config(
-#         Constant(1e-5), 1, 1, do_symmetry=True, 
-#         variational_rnn=True, rnn_width=32, 
-#         deep_spread=False, vae_channels=[16, 32, 64], 
-#         vvrnn=False, vvrnn_static=-20, rnn_min_context=5, 
-#     )), 
-# ]
 
 EXPERIMENTS = [
-    ('-10', Config(
+    ('3', Config(
         Constant(1e-5), 1, 1, do_symmetry=True, 
         variational_rnn=True, rnn_width=32, 
         deep_spread=False, vae_channels=[16, 32, 64], 
-        vvrnn=False, vvrnn_static=-10, rnn_min_context=7, 
+        vvrnn=False, vvrnn_static=-5, rnn_min_context=3, 
     )), 
-    ('-5', Config(
+    ('7', Config(
         Constant(1e-5), 1, 1, do_symmetry=True, 
         variational_rnn=True, rnn_width=32, 
         deep_spread=False, vae_channels=[16, 32, 64], 
         vvrnn=False, vvrnn_static=-5, rnn_min_context=7, 
-    )), 
-    ('-2', Config(
-        Constant(1e-5), 1, 1, do_symmetry=True, 
-        variational_rnn=True, rnn_width=32, 
-        deep_spread=False, vae_channels=[16, 32, 64], 
-        vvrnn=False, vvrnn_static=-2, rnn_min_context=7, 
     )), 
 ]

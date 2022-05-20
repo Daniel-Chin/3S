6ecbd5b2ec8234cfb216b80cd3d3ae1609862580
Daniel Chin <daniel.chin@nyu.edu>
2022-05-20 14:46:30 -0400

    back_compat

--- bounce/shared.py
+++ bounce/shared.py
@@ -1,8 +1,9 @@
+from argparse import ArgumentError
 import os
-from collections import namedtuple
 import torch
 import numpy as np
 from PIL import Image
+from betaSched import Constant
 
 TRAIN_PATH    = './dataset/train'
 VALIDATE_PATH = './dataset/validate'
@@ -11,11 +12,46 @@ RESOLUTION = 32
 IMG_N_CHANNELS = 3
 TRAIN_SET_SIZE, VALIDATE_SET_SIZE = 256, 64
 
-Config = namedtuple('Config', [
-    'beta', 'vae_loss_coef', 'rnn_loss_coef', 'do_symmetry', 
-    'variational_rnn', 'rnn_width', 'deep_spread', 
-    'vae_channels', 'vvrnn', 'vvrnn_static', 'rnn_min_context', 
-])
+class Config:
+    keys = [
+        'beta', 'vae_loss_coef', 'rnn_loss_coef', 
+        'do_symmetry', 'variational_rnn', 'rnn_width', 
+        'deep_spread', 'vae_channels', 'vvrnn', 'vvrnn_static', 
+        'rnn_min_context', 
+    ]
+    defaults=[
+        Constant(1e-5), 1, 1, 
+        True, True, 32, 
+        False, [16, 32, 64], False, -5, 
+        7, 
+    ]
+    def __init__(self, *a, **kw):
+        for k, v in zip(self.keys, self.defaults):
+            self.__setattr__(k, v)
+        for i, v in enumerate(a):
+            self.__setattr__(self.keys[i], v)
+        self.prefix_len = len(a)
+        for k, v in kw.items():
+            key_i = self.keys.index(k)
+            if key_i < self.prefix_len:
+                raise ArgumentError(
+                    'keyword arg goes backwards.', 
+                )
+            if key_i == self.prefix_len:
+                self.prefix_len += 1
+            self.__setattr__(k, v)
+    
+    def getPrefix(self):
+        return [
+            self.__getattribute__(k) 
+            for k in self.keys[:self.prefix_len]
+        ]
+    
+    def __repr__(self):
+        params = []
+        for k in self.keys:
+            params.append(f'{k}={self.__getattribute__(k) }')
+        return f'Config({", ".join(params)})'
 
 def torch2PIL(torchImg: torch.Tensor):
     return Image.fromarray((
@@ -29,7 +65,7 @@ def renderExperimentPath(
 ):
     return os.path.join(
         EXPERIMENTS_PATH, f'''{(
-            rand_init_i, *config, 
+            rand_init_i, *config.getPrefix(), 
         )}'''
     )
 

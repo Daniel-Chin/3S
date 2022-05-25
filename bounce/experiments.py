d7ccc1c92cac0d7de5378f9ca7673c20c934f57f
Daniel Chin <daniel.chin@nyu.edu>
2022-05-25 13:21:55 -0400

    exp

--- bounce/experiments.py
+++ bounce/experiments.py
@@ -4,20 +4,6 @@ from betaSched import Constant, WarmUp, Osc
 RAND_INIT_TIMES = 1
 
 EXPERIMENTS = [
-    ('1', Config(
-        Constant(1e-5), 1, 1, do_symmetry=True, 
-        variational_rnn=True, rnn_width=32, 
-        deep_spread=False, vae_channels=[16, 32, 64], 
-        vvrnn=False, vvrnn_static=-25, rnn_min_context=4, 
-        z_pred_loss_coef=1, 
-    )), 
-    ('.1', Config(
-        Constant(1e-5), 1, 1, do_symmetry=True, 
-        variational_rnn=True, rnn_width=32, 
-        deep_spread=False, vae_channels=[16, 32, 64], 
-        vvrnn=False, vvrnn_static=-25, rnn_min_context=4, 
-        z_pred_loss_coef=.1, 
-    )), 
     ('.01', Config(
         Constant(1e-5), 1, 1, do_symmetry=True, 
         variational_rnn=True, rnn_width=32, 
@@ -25,11 +11,18 @@ EXPERIMENTS = [
         vvrnn=False, vvrnn_static=-25, rnn_min_context=4, 
         z_pred_loss_coef=.01, 
     )), 
-    ('0', Config(
+    ('.006', Config(
         Constant(1e-5), 1, 1, do_symmetry=True, 
         variational_rnn=True, rnn_width=32, 
         deep_spread=False, vae_channels=[16, 32, 64], 
         vvrnn=False, vvrnn_static=-25, rnn_min_context=4, 
-        z_pred_loss_coef=0, 
+        z_pred_loss_coef=.006, 
+    )), 
+    ('.003', Config(
+        Constant(1e-5), 1, 1, do_symmetry=True, 
+        variational_rnn=True, rnn_width=32, 
+        deep_spread=False, vae_channels=[16, 32, 64], 
+        vvrnn=False, vvrnn_static=-25, rnn_min_context=4, 
+        z_pred_loss_coef=.003, 
     )), 
 ]

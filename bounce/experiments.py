76c9e564e4675fae7c7d2f0e29c3631213af2f8f
Daniel Chin <daniel.chin@nyu.edu>
2022-05-27 09:08:08 -0400

    exp

--- bounce/experiments.py
+++ bounce/experiments.py
@@ -11,18 +11,11 @@ EXPERIMENTS = [
         vvrnn=False, vvrnn_static=-25, rnn_min_context=4, 
         z_pred_loss_coef=.01, 
     )), 
-    ('.006', Config(
+    ('.02', Config(
         Constant(1e-5), 1, 1, do_symmetry=True, 
         variational_rnn=True, rnn_width=32, 
         deep_spread=False, vae_channels=[16, 32, 64], 
         vvrnn=False, vvrnn_static=-25, rnn_min_context=4, 
-        z_pred_loss_coef=.006, 
-    )), 
-    ('.003', Config(
-        Constant(1e-5), 1, 1, do_symmetry=True, 
-        variational_rnn=True, rnn_width=32, 
-        deep_spread=False, vae_channels=[16, 32, 64], 
-        vvrnn=False, vvrnn_static=-25, rnn_min_context=4, 
-        z_pred_loss_coef=.003, 
+        z_pred_loss_coef=.02, 
     )), 
 ]

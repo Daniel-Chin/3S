cf1acb8cd99f9e3fe6b7cc90f67c4f4160a5bdcb
Daniel Chin <daniel.chin@nyu.edu>
2022-06-09 11:53:32 +0400

    xuanjie unit diff

--- bounce/experiments.py
+++ bounce/experiments.py
@@ -9,7 +9,7 @@ EXPERIMENTS = [
         variational_rnn=True, rnn_width=32, 
         deep_spread=False, vae_channels=[16, 32, 64], 
         vvrnn=False, vvrnn_static=-25, rnn_min_context=4, 
-        z_pred_loss_coef=.02, 
+        z_pred_loss_coef=.005, 
         T=0, R=0, TR=1, I=0, 
     )), 
     ('T=1 R=1', Config(
@@ -17,7 +17,7 @@ EXPERIMENTS = [
         variational_rnn=True, rnn_width=32, 
         deep_spread=False, vae_channels=[16, 32, 64], 
         vvrnn=False, vvrnn_static=-25, rnn_min_context=4, 
-        z_pred_loss_coef=.02, 
+        z_pred_loss_coef=.005, 
         T=1, R=1, TR=0, I=0, 
     )), 
     ('T=4 R=4 I=1', Config(
@@ -25,7 +25,7 @@ EXPERIMENTS = [
         variational_rnn=True, rnn_width=32, 
         deep_spread=False, vae_channels=[16, 32, 64], 
         vvrnn=False, vvrnn_static=-25, rnn_min_context=4, 
-        z_pred_loss_coef=.02, 
+        z_pred_loss_coef=.005, 
         T=4, R=4, TR=0, I=1, 
     )), 
 ]
@@ -36,7 +36,7 @@ EXPERIMENTS = [
         variational_rnn=True, rnn_width=32, 
         deep_spread=False, vae_channels=[16, 32, 64], 
         vvrnn=False, vvrnn_static=-25, rnn_min_context=4, 
-        z_pred_loss_coef=.02, 
+        z_pred_loss_coef=.005, 
         T=0, R=0, TR=1, I=0, lr=0.001, 
     )), 
     ('0.0001', Config(
@@ -44,7 +44,7 @@ EXPERIMENTS = [
         variational_rnn=True, rnn_width=32, 
         deep_spread=False, vae_channels=[16, 32, 64], 
         vvrnn=False, vvrnn_static=-25, rnn_min_context=4, 
-        z_pred_loss_coef=.02, 
+        z_pred_loss_coef=.005, 
         T=0, R=0, TR=1, I=0, lr=0.0001, 
     )), 
 ]

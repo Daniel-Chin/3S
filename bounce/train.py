f839da75647662f0b37adef3e244421b5bbee7f5
Daniel Chin <daniel.chin@nyu.edu>
2022-06-16 08:08:59 +0400

    check gpu name

--- bounce/train.py
+++ bounce/train.py
@@ -33,6 +33,11 @@ CPU  = torch.device("cpu")
 if HAS_CUDA:
     DEVICE = CUDA
     print('We have CUDA.')
+    gpu_name = torch.cuda.get_device_name(
+        torch.cuda.current_device(), 
+    )
+    with open('gpu_name.txt', 'w') as f:
+        print(gpu_name, file=f)
 else:
     DEVICE = CPU
     print("We DON'T have CUDA.")

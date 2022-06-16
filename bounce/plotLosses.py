2513f7e352dac368cbf76b9f1f11596ce33284c3
Daniel Chin <daniel.chin@nyu.edu>
2022-06-16 06:07:56 +0400

    print losses

---
+++ bounce/plotLosses.py
@@ -0,0 +1,64 @@
+import os
+from os import path
+import importlib.util
+
+from matplotlib import pyplot as plt
+
+from shared import *
+
+EXP_PATH = 'C:/Users/iGlop/d/symmetry/danRepo/bounce/results/z_pred_loss_6'
+
+AVERAGE_OVER = 100
+
+def getExp():
+    spec = importlib.util.spec_from_file_location(
+        "experiments", path.abspath('experiments.py'), 
+    )
+    experiments = importlib.util.module_from_spec(spec)
+    spec.loader.exec_module(experiments)
+    return experiments
+
+def main():
+    os.chdir(EXP_PATH)
+    experiments = getExp()
+    for i, (exp_name, config) in enumerate(
+        experiments.EXPERIMENTS, 
+    ):
+        print(exp_name)
+        print(config)
+        for rand_init_i in range(experiments.RAND_INIT_TIMES):
+            print('rand init', rand_init_i)
+            exp_path = renderExperimentPath(rand_init_i, config)
+            losses = extract(exp_path)
+            plt.plot(losses, c='rgbky'[i], label=exp_name)
+    plt.legend()
+    plt.show()
+
+PREFIX = '  validate_recon__loss = '
+PREFIX_LEN = len(PREFIX)
+def extract(exp_path):
+    losses = []
+    group_sum = 0
+    group_size = 0
+    def pop():
+        nonlocal group_sum, group_size
+        losses.append(group_sum / group_size)
+        print(
+            'epoch', len(losses) * AVERAGE_OVER, 
+            end='\r', flush=True, 
+        )
+        group_sum = 0
+        group_size = 0
+    with open(path.join(exp_path, 'losses.log')) as f:
+        for i, line in enumerate(f):
+            if i % 9 == 2:
+                if not line.startswith(PREFIX):
+                    from console import console
+                    console({**globals(), **locals()})
+                group_sum += float(line[PREFIX_LEN:])
+                group_size += 1
+                if group_size == AVERAGE_OVER:
+                    pop()
+    return losses
+
+main()

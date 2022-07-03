aefa6e54cac762176424f8d96fd6d2b6d67dc6da
Daniel Chin <daniel.chin@nyu.edu>
2022-07-03 09:32:06 +0400

    exp

--- bounce/evalDecoder.py
+++ bounce/evalDecoder.py
@@ -11,7 +11,7 @@ from vae import LATENT_DIM, VAE
 from loadModels import loadModels
 from train import DEVICE
 
-EXP_PATH = 'C:/Users/iGlop/d/symmetry/danRepo/bounce/results/residual'
+EXP_PATH = 'C:/Users/iGlop/d/symmetry/danRepo/bounce/results/TRI_6'
 
 RADIUS = 2
 TICK_INTERVAL = 0.05

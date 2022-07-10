2ab89c3b26db65ab494eebb727d7f386edb2e1f6
Daniel Chin <daniel.chin@nyu.edu>
2022-07-10 08:44:12 +0400

    exp

--- bounce/experiments.py
+++ bounce/experiments.py
@@ -52,4 +52,22 @@ EXPERIMENTS = [
         T=0, R=0, TR=1, I=0, lr=0.001, residual=True, 
         grad_clip=.03, 
     )), 
+    ('-3', Config(
+        Constant(1e-3), 1, 1, do_symmetry=True, 
+        variational_rnn=True, rnn_width=32, 
+        deep_spread=False, vae_channels=[16, 32, 64], 
+        vvrnn=False, vvrnn_static=-25, rnn_min_context=4, 
+        z_pred_loss_coef=.005, 
+        T=0, R=0, TR=1, I=0, lr=0.001, residual=True, 
+        grad_clip=.03, 
+    )), 
+    ('-2', Config(
+        Constant(1e-3), 1, 1, do_symmetry=True, 
+        variational_rnn=True, rnn_width=32, 
+        deep_spread=False, vae_channels=[16, 32, 64], 
+        vvrnn=False, vvrnn_static=-25, rnn_min_context=4, 
+        z_pred_loss_coef=.005, 
+        T=0, R=0, TR=1, I=0, lr=0.001, residual=True, 
+        grad_clip=.03, 
+    )), 
 ]
--- bounce/plotLosses.py
+++ bounce/plotLosses.py
@@ -7,7 +7,7 @@ from matplotlib import pyplot as plt
 
 from shared import *
 
-EXP_PATH = 'C:/Users/iGlop/d/symmetry/danRepo/bounce/results/TRI_8'
+EXP_PATH = 'C:/Users/iGlop/d/symmetry/danRepo/bounce/results/beta'
 
 AVERAGE_OVER = 300
 START = 20
@@ -94,9 +94,9 @@ def extract(exp_path, exp_name, exp_i):
         loss_types = [
             # validate_recon__loss, 
             # train____recon__loss, 
-            # validate_kld____loss, 
+            validate_kld____loss, 
             # validate_img_pred_loss, 
-            train____img_pred_loss, 
+            # train____img_pred_loss, 
         ]
     epochs = []
     lossAccs = {x: LossAcc([]) for x in loss_types}

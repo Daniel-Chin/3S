209f80a8acd345b1aeb5e07f4e182ff0a507261d
Daniel Chin <daniel.chin@nyu.edu>
2022-09-20 20:48:07 +0400

    exp

--- bounce/experiments.py
+++ bounce/experiments.py
@@ -1,7 +1,7 @@
 from train import Config
 from betaSched import Constant, WarmUp, Osc
 
-RAND_INIT_TIMES = 1
+RAND_INIT_TIMES = 2
 
 # Symmetry is off!
 
@@ -67,30 +67,6 @@ EXPERIMENTS = [
         teacher_forcing_duration=1e6, 
     )), 
 ]
-
-EXPERIMENTS = [
-    ('1', Config(
-        Constant(1e-5), 1, 1, do_symmetry=False, 
-        variational_rnn=True, rnn_width=128, 
-        deep_spread=False, vae_channels=[16, 32, 64], 
-        vvrnn=False, vvrnn_static=-25, rnn_min_context=4, 
-        z_pred_loss_coef=.005, 
-        T=0, R=0, TR=0, I=1, lr=0.001, residual=True, 
-        grad_clip=.03, BCE_not_MSE=False, 
-        teacher_forcing_duration=0, 
-    )), 
-    ('.5', Config(
-        Constant(1e-5), 1, .5, do_symmetry=False, 
-        variational_rnn=True, rnn_width=128, 
-        deep_spread=False, vae_channels=[16, 32, 64], 
-        vvrnn=False, vvrnn_static=-25, rnn_min_context=4, 
-        z_pred_loss_coef=.005, 
-        T=0, R=0, TR=0, I=1, lr=0.001, residual=True, 
-        grad_clip=.03, BCE_not_MSE=False, 
-        teacher_forcing_duration=0, 
-    )), 
-]
-
 # supervised rnn
 # EXPERIMENTS = [
 #     ('.005', Config(
@@ -120,44 +96,44 @@ EXPERIMENTS = [
 # ]
 
 # supervised rnn supervised vae
-# EXPERIMENTS = [
-#     ('128', Config(
-#         beta=Constant(0), vae_loss_coef=0, 
-#         img_pred_loss_coef=0, do_symmetry=False, 
-#         variational_rnn=True, rnn_width=128, 
-#         deep_spread=False, vae_channels=[16, 32, 64], 
-#         vvrnn=False, vvrnn_static=-25, rnn_min_context=4, 
-#         z_pred_loss_coef=1, 
-#         T=0, R=0, TR=0, I=1, lr=0.001, residual=True, 
-#         grad_clip=.03, BCE_not_MSE=False, 
-#         teacher_forcing_duration=0, 
-#         supervised_rnn=True, skip_vae=False, 
-#         supervised_vae=True, vae_supervision_loss_coef=1, 
-#     )), 
-#     ('64', Config(
-#         beta=Constant(0), vae_loss_coef=0, 
-#         img_pred_loss_coef=0, do_symmetry=False, 
-#         variational_rnn=True, rnn_width=64, 
-#         deep_spread=False, vae_channels=[16, 32, 64], 
-#         vvrnn=False, vvrnn_static=-25, rnn_min_context=4, 
-#         z_pred_loss_coef=1, 
-#         T=0, R=0, TR=0, I=1, lr=0.001, residual=True, 
-#         grad_clip=.03, BCE_not_MSE=False, 
-#         teacher_forcing_duration=0, 
-#         supervised_rnn=True, skip_vae=False, 
-#         supervised_vae=True, vae_supervision_loss_coef=1, 
-#     )), 
-#     ('32', Config(
-#         beta=Constant(0), vae_loss_coef=0, 
-#         img_pred_loss_coef=0, do_symmetry=False, 
-#         variational_rnn=True, rnn_width=32, 
-#         deep_spread=False, vae_channels=[16, 32, 64], 
-#         vvrnn=False, vvrnn_static=-25, rnn_min_context=4, 
-#         z_pred_loss_coef=1, 
-#         T=0, R=0, TR=0, I=1, lr=0.001, residual=True, 
-#         grad_clip=.03, BCE_not_MSE=False, 
-#         teacher_forcing_duration=0, 
-#         supervised_rnn=True, skip_vae=False, 
-#         supervised_vae=True, vae_supervision_loss_coef=1, 
-#     )), 
-# ]
+EXPERIMENTS = [
+    ('128', Config(
+        beta=Constant(0), vae_loss_coef=0, 
+        img_pred_loss_coef=0, do_symmetry=False, 
+        variational_rnn=True, rnn_width=128, 
+        deep_spread=False, vae_channels=[16, 32, 64], 
+        vvrnn=False, vvrnn_static=-25, rnn_min_context=4, 
+        z_pred_loss_coef=1, 
+        T=0, R=0, TR=0, I=1, lr=0.001, residual=True, 
+        grad_clip=.03, BCE_not_MSE=False, 
+        teacher_forcing_duration=0, 
+        supervised_rnn=True, skip_vae=False, 
+        supervised_vae=True, vae_supervision_loss_coef=1, 
+    )), 
+    ('64', Config(
+        beta=Constant(0), vae_loss_coef=0, 
+        img_pred_loss_coef=0, do_symmetry=False, 
+        variational_rnn=True, rnn_width=64, 
+        deep_spread=False, vae_channels=[16, 32, 64], 
+        vvrnn=False, vvrnn_static=-25, rnn_min_context=4, 
+        z_pred_loss_coef=1, 
+        T=0, R=0, TR=0, I=1, lr=0.001, residual=True, 
+        grad_clip=.03, BCE_not_MSE=False, 
+        teacher_forcing_duration=0, 
+        supervised_rnn=True, skip_vae=False, 
+        supervised_vae=True, vae_supervision_loss_coef=1, 
+    )), 
+    ('32', Config(
+        beta=Constant(0), vae_loss_coef=0, 
+        img_pred_loss_coef=0, do_symmetry=False, 
+        variational_rnn=True, rnn_width=32, 
+        deep_spread=False, vae_channels=[16, 32, 64], 
+        vvrnn=False, vvrnn_static=-25, rnn_min_context=4, 
+        z_pred_loss_coef=1, 
+        T=0, R=0, TR=0, I=1, lr=0.001, residual=True, 
+        grad_clip=.03, BCE_not_MSE=False, 
+        teacher_forcing_duration=0, 
+        supervised_rnn=True, skip_vae=False, 
+        supervised_vae=True, vae_supervision_loss_coef=1, 
+    )), 
+]

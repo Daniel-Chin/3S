ecdc2c816a490beb227dd5ed5b46418756216ce8
Daniel Chin <daniel.chin@nyu.edu>
2022-10-11 11:04:57 +0400

    give profiler to lossloger

--- bounce_clean/one_epoch.py
+++ bounce_clean/one_epoch.py
@@ -77,6 +77,7 @@ def oneEpoch(
                     ('grad_norm', grad_norm), 
                     *extra_logs, 
                 ], 
+                profiler, 
             )
 
     vae.eval()
@@ -100,6 +101,7 @@ def oneEpoch(
                         ('grad_norm', 0), 
                         *extra_logs, 
                     ], 
+                    profiler, 
                 )
 
         with profiler('gif'):

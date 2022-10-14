b2217f7d9f2f48bd757268d868f8d33a179c12b9
Daniel Chin <daniel.chin@nyu.edu>
2022-10-14 13:42:47 +0400

    use str constant; use

--- bounce_clean/plot_losses.py
+++ bounce_clean/plot_losses.py
@@ -2,17 +2,19 @@ from os import path
 
 from matplotlib import pyplot as plt
 from torchWork.plot_losses import plotLosses, LossType
+from torchWork.experiment_control import EXPERIMENT_PY_FILENAME
 
 fig = plotLosses(
     path.join('./experiments/', '''
 supervised_rnn_width_2022_Oct_13_22;48;34
-'''.strip(), 'experiment.py'), 
+'''.strip(), EXPERIMENT_PY_FILENAME), 
     [
         # LossType('validate', 'loss_root.supervise.vae'), 
         # LossType('validate', 'loss_root.supervise.rnn'), 
 
         LossType('validate', 'loss_root.supervise.vae.encode'), 
-        LossType('validate', 'loss_root.supervise.vae.decode'), 
+        LossType('train', 'loss_root.supervise.vae.encode'), 
+        # LossType('validate', 'loss_root.supervise.vae.decode'), 
     ], 
     average_over=300, epoch_start=3000, 
 )

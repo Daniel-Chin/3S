5e82ba40dd6c77f70cf7ad223e349eb1f617f36a
Daniel Chin <daniel.chin@nyu.edu>
2022-10-16 18:48:17 +0400

    a

--- bounce_clean/plot_losses.py
+++ bounce_clean/plot_losses.py
@@ -6,15 +6,17 @@ from torchWork.experiment_control import EXPERIMENT_PY_FILENAME
 
 fig = plotLosses(
     path.join('./experiments/', '''
-supervised_rnn_width_2022_Oct_13_22;48;34
+supervised_rnn_width_2022_Oct_14_18;44;17
 '''.strip(), EXPERIMENT_PY_FILENAME), 
     [
         # LossType('validate', 'loss_root.supervise.vae'), 
         # LossType('validate', 'loss_root.supervise.rnn'), 
 
-        LossType('validate', 'loss_root.supervise.vae.encode'), 
-        LossType('train', 'loss_root.supervise.vae.encode'), 
+        # LossType('validate', 'loss_root.supervise.vae.encode'), 
         # LossType('validate', 'loss_root.supervise.vae.decode'), 
+        LossType('train', 'loss_root.supervise.vae.encode'), 
+        LossType('train', 'loss_root.supervise.vae.decode'), 
+        LossType('train', 'loss_root.self_recon'), 
     ], 
     average_over=300, epoch_start=3000, 
 )

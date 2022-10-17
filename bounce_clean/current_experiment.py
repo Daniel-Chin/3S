6c6dbba55483eaa09071fc827e30c083fecc3173
Daniel Chin <daniel.chin@nyu.edu>
2022-10-17 19:04:34 +0400

    exp

--- bounce_clean/current_experiment.py
+++ bounce_clean/current_experiment.py
@@ -3,7 +3,7 @@ from torchWork import LossWeightTree, ExperimentGroup, DEVICE
 
 from shared import *
 
-EXP_NAME = 'teacher_forcing_duration'
+EXP_NAME = 'symm_rnn_width'
 N_RAND_INITS = 1
 
 class MyExpGroup(ExperimentGroup):
@@ -12,8 +12,7 @@ class MyExpGroup(ExperimentGroup):
     
     @lru_cache(1)
     def name(self):
-        x = self.hyperParams.teacher_forcing_duration
-        return f'''teacher_f_time={x:.1E}'''
+        return f'rnn_width={self.hyperParams.rnn_width}'
 
 GROUPS = []
 
@@ -35,10 +34,10 @@ hP.lossWeightTree = LossWeightTree('total', 1, [
     ]), 
 ])
 hP.lr = 0.001
-hP.I = 1
+hP.I = 0
 hP.T = 0
 hP.R = 0
-hP.TR = 0
+hP.TR = 1
 hP.supervise_rnn = False
 hP.supervise_vae = False
 hP.variational_rnn = True
@@ -54,127 +53,7 @@ hP.grad_clip = .03
 hP.optim_name = 'adam'
 hP.image_loss = 'mse'
 hP.train_set_size = 256
-hP.teacher_forcing_duration = 0
-hP.ready()
-
-hP = HyperParams()
-GROUPS.append(MyExpGroup(hP))
-hP.lossWeightTree = LossWeightTree('total', 1, [
-    LossWeightTree('self_recon', 1, None), 
-    LossWeightTree('kld', 1e-5, None), 
-    LossWeightTree('predict', 1, [
-        LossWeightTree('z', .005, None), 
-        LossWeightTree('image', 1, None), 
-    ]), 
-    LossWeightTree('supervise', 0, [
-        LossWeightTree('rnn', 0, None), 
-        LossWeightTree('vae', 0, [
-            LossWeightTree('encode', 0, None), 
-            LossWeightTree('decode', 0, None), 
-        ]), 
-    ]), 
-])
-hP.lr = 0.001
-hP.I = 1
-hP.T = 0
-hP.R = 0
-hP.TR = 0
-hP.supervise_rnn = False
-hP.supervise_vae = False
-hP.variational_rnn = True
-hP.vvrnn = False
-hP.vvrnn_static = -25
-hP.rnn_min_context = 4
-hP.rnn_width = 32
-hP.residual = True
-hP.vae_channels = [16, 32, 64]
-hP.deep_spread = False
-hP.batch_size = 128
-hP.grad_clip = .03
-hP.optim_name = 'adam'
-hP.image_loss = 'mse'
-hP.train_set_size = 256
-hP.teacher_forcing_duration = 1e2
-hP.ready()
-
-hP = HyperParams()
-GROUPS.append(MyExpGroup(hP))
-hP.lossWeightTree = LossWeightTree('total', 1, [
-    LossWeightTree('self_recon', 1, None), 
-    LossWeightTree('kld', 1e-5, None), 
-    LossWeightTree('predict', 1, [
-        LossWeightTree('z', .005, None), 
-        LossWeightTree('image', 1, None), 
-    ]), 
-    LossWeightTree('supervise', 0, [
-        LossWeightTree('rnn', 0, None), 
-        LossWeightTree('vae', 0, [
-            LossWeightTree('encode', 0, None), 
-            LossWeightTree('decode', 0, None), 
-        ]), 
-    ]), 
-])
-hP.lr = 0.001
-hP.I = 1
-hP.T = 0
-hP.R = 0
-hP.TR = 0
-hP.supervise_rnn = False
-hP.supervise_vae = False
-hP.variational_rnn = True
-hP.vvrnn = False
-hP.vvrnn_static = -25
-hP.rnn_min_context = 4
-hP.rnn_width = 32
-hP.residual = True
-hP.vae_channels = [16, 32, 64]
-hP.deep_spread = False
-hP.batch_size = 128
-hP.grad_clip = .03
-hP.optim_name = 'adam'
-hP.image_loss = 'mse'
-hP.train_set_size = 256
-hP.teacher_forcing_duration = 1e3
-hP.ready()
-
-hP = HyperParams()
-GROUPS.append(MyExpGroup(hP))
-hP.lossWeightTree = LossWeightTree('total', 1, [
-    LossWeightTree('self_recon', 1, None), 
-    LossWeightTree('kld', 1e-5, None), 
-    LossWeightTree('predict', 1, [
-        LossWeightTree('z', .005, None), 
-        LossWeightTree('image', 1, None), 
-    ]), 
-    LossWeightTree('supervise', 0, [
-        LossWeightTree('rnn', 0, None), 
-        LossWeightTree('vae', 0, [
-            LossWeightTree('encode', 0, None), 
-            LossWeightTree('decode', 0, None), 
-        ]), 
-    ]), 
-])
-hP.lr = 0.001
-hP.I = 1
-hP.T = 0
-hP.R = 0
-hP.TR = 0
-hP.supervise_rnn = False
-hP.supervise_vae = False
-hP.variational_rnn = True
-hP.vvrnn = False
-hP.vvrnn_static = -25
-hP.rnn_min_context = 4
-hP.rnn_width = 32
-hP.residual = True
-hP.vae_channels = [16, 32, 64]
-hP.deep_spread = False
-hP.batch_size = 128
-hP.grad_clip = .03
-hP.optim_name = 'adam'
-hP.image_loss = 'mse'
-hP.train_set_size = 256
-hP.teacher_forcing_duration = 1e4
+hP.teacher_forcing_duration = 1e+5
 hP.ready()
 
 
@@ -196,17 +75,17 @@ hP.lossWeightTree = LossWeightTree('total', 1, [
     ]), 
 ])
 hP.lr = 0.001
-hP.I = 1
+hP.I = 0
 hP.T = 0
 hP.R = 0
-hP.TR = 0
+hP.TR = 1
 hP.supervise_rnn = False
 hP.supervise_vae = False
 hP.variational_rnn = True
 hP.vvrnn = False
 hP.vvrnn_static = -25
 hP.rnn_min_context = 4
-hP.rnn_width = 32
+hP.rnn_width = 16
 hP.residual = True
 hP.vae_channels = [16, 32, 64]
 hP.deep_spread = False
@@ -215,46 +94,5 @@ hP.grad_clip = .03
 hP.optim_name = 'adam'
 hP.image_loss = 'mse'
 hP.train_set_size = 256
-hP.teacher_forcing_duration = 1e5
-hP.ready()
-
-
-hP = HyperParams()
-GROUPS.append(MyExpGroup(hP))
-hP.lossWeightTree = LossWeightTree('total', 1, [
-    LossWeightTree('self_recon', 1, None), 
-    LossWeightTree('kld', 1e-5, None), 
-    LossWeightTree('predict', 1, [
-        LossWeightTree('z', .005, None), 
-        LossWeightTree('image', 1, None), 
-    ]), 
-    LossWeightTree('supervise', 0, [
-        LossWeightTree('rnn', 0, None), 
-        LossWeightTree('vae', 0, [
-            LossWeightTree('encode', 0, None), 
-            LossWeightTree('decode', 0, None), 
-        ]), 
-    ]), 
-])
-hP.lr = 0.001
-hP.I = 1
-hP.T = 0
-hP.R = 0
-hP.TR = 0
-hP.supervise_rnn = False
-hP.supervise_vae = False
-hP.variational_rnn = True
-hP.vvrnn = False
-hP.vvrnn_static = -25
-hP.rnn_min_context = 4
-hP.rnn_width = 32
-hP.residual = True
-hP.vae_channels = [16, 32, 64]
-hP.deep_spread = False
-hP.batch_size = 128
-hP.grad_clip = .03
-hP.optim_name = 'adam'
-hP.image_loss = 'mse'
-hP.train_set_size = 256
-hP.teacher_forcing_duration = 1e6
+hP.teacher_forcing_duration = 1e+5
 hP.ready()

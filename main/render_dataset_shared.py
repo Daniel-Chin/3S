605d57aa69850bcf744d9b6dbc3afde2f35b0939
Daniel Chin <daniel.chin@nyu.edu>
2022-10-22 09:15:44 +0400

    share renderer commonality

---
+++ main/render_dataset_shared.py
@@ -0,0 +1,228 @@
+from os import path
+import time
+from typing import List
+import json
+from abc import ABCMeta, abstractmethod
+
+import numpy as np
+from OpenGL.GL import *
+from OpenGL.GLU import *
+from OpenGL.GLUT import *
+import cv2
+from PIL import Image
+
+from shared import *
+from physics_shared import *
+
+__all__ = [
+    'MODE_LOCATE', 'MODE_OBV_ONLY', 'MODE_MAKE_IMG', 
+    'BallViewer', 
+]
+
+WIN_W = 320
+WIN_H = 320
+DRAW_GIRD = False
+
+VIEW = np.array([-0.8, 0.8, -0.8, 0.8, 1.0, 100.0])  # 视景体的left/right/bottom/top/near/far六个面
+SCALE_K = np.array([1.0, 1.0, 1.0])  # 模型缩放比例
+EYE_UP = np.array([0.0, 0.0, 1.0])  # 定义对观察者而言的上方（默认y轴的正方向）
+
+MODE_LOCATE = 'locate'
+MODE_OBV_ONLY = 'obv_only'
+MODE_MAKE_IMG = 'make_img'
+
+class BallViewer(metaclass=ABCMeta):
+    def __init__(
+        self, running_mode, ball_radius, 
+        eye, look_at, SPF, 
+    ) -> None:
+        self.running_mode = running_mode
+        self.ball_radius = ball_radius
+        self.eye = eye
+        self.look_at = look_at
+        self.SPF = SPF
+        self.trajectory: List[List[Body]] = None
+        self.stage: int = None
+        self.output_i = 0
+        self.render_or_screenshot = 0
+        self.frames: List[Image.Image] = None
+        self.reset()
+        self.initGlut()
+    
+    def reset(self):
+        self.trajectory = self.getTrajectory()
+        self.stage = 0
+        self.frames = []
+    
+    @abstractmethod
+    def getTrajectory(self) -> List[List[Body]]:
+        raise NotImplemented
+
+    def loop(self):
+        if DRAW_GIRD:
+            self.drawGrid()
+        if self.render_or_screenshot == 0:
+            if self.running_mode is MODE_LOCATE:
+                self.locate_with_ball()
+                return
+            
+            if self.stage >= SEQ_LEN:
+                if self.running_mode is MODE_MAKE_IMG:
+                    self.saveVideo()
+                self.reset()
+                self.output_i += 1
+            
+            bodies = self.trajectory[self.stage]
+
+            self.makeBodies(*bodies)
+            
+            # step
+            self.stage += 1
+            if self.running_mode is MODE_OBV_ONLY:
+                time.sleep(self.SPF)
+        else:
+            bodies = self.trajectory[self.stage - 1]
+            self.makeBodies(*bodies)
+            if self.running_mode is MODE_MAKE_IMG:
+                self.frames.append(screenShot())
+        
+        self.render_or_screenshot = 1 - self.render_or_screenshot
+    
+    def makeBall(self, x, y, z, r=1, color3f=(0, 1, 0)):
+        glPushMatrix()
+        glColor3f(* color3f)
+        glTranslatef(x, y, z)
+        quad = gluNewQuadric()
+        gluSphere(quad, r, 90, 90)
+        gluDeleteQuadric(quad)
+        glPopMatrix()
+    
+    def makeBodies(self, *bodies: Body):
+        for body, color in zip(bodies, (
+            (0, 1, 0), 
+            (1, 0, 1), 
+        )):
+            self.makeBall(*body.position, self.ball_radius, color)
+
+    def saveVideo(self):
+        # self.frames[0].save(
+        #     f'{self.output_i}.gif', save_all=True, 
+        #     append_images=self.frames[1:], 
+        #     duration=GIF_INTERVAL, loop=0, 
+        # )
+        # GIF image compression is uncontrollable
+        os.makedirs(str(self.output_i), exist_ok=True)
+        os.chdir(str(self.output_i))
+        with open(TRAJ_FILENAME, 'w') as f:
+            trajectory_json = []
+            for bodies in self.trajectory:
+                bodies_json = []
+                trajectory_json.append(bodies_json)
+                for body in bodies:
+                    bodies_json.append(body.toJSON())
+            json.dump(trajectory_json, f)
+        for i, img in enumerate(self.frames):
+            img.save(f'{i}.png')
+        os.chdir('..')
+
+    @abstractmethod
+    def locate_with_ball(self):
+        raise NotImplemented
+
+    def drawGrid(self):
+        glLineWidth(3)
+        glBegin(GL_LINES)
+        glColor4f(0.0, 0.0, 0.0, 1)  # 设置当前颜色为黑色不透明
+        for i in np.linspace(-15, 15, 19):
+            glVertex3f(i, -100, 0)
+            glVertex3f(i, +100, 0)
+            glVertex3f(-100, i, 0)
+            glVertex3f(+100, i, 0)
+        glEnd()
+
+    def initGlut(self):
+        glutInit()
+        displayMode = GLUT_DOUBLE | GLUT_ALPHA | GLUT_DEPTH
+        glutInitDisplayMode(displayMode)
+
+        glutInitWindowSize(WIN_W, WIN_H)
+        glutInitWindowPosition(300, 50)
+        glutCreateWindow('Ball Throwing Simulation')
+
+        # 初始化画布
+        glClearColor(0.4, 0.4, 0.4, 1.0)  # 设置画布背景色。注意：这里必须是4个参数
+        glEnable(GL_DEPTH_TEST)  # 开启深度测试，实现遮挡关系
+        glDepthFunc(GL_LEQUAL)  # 设置深度测试函数（GL_LEQUAL只是选项之一）-
+        glEnable(GL_LIGHT0)  # 启用0号光源
+        glLightfv(GL_LIGHT0, GL_POSITION, GLfloat_4(0, 1, 4, 0))  # 设置光源的位置
+        glLightfv(GL_LIGHT0, GL_SPOT_DIRECTION, GLfloat_3(0, 0, -1))  # 设置光源的照射方向
+        glColorMaterial(GL_FRONT_AND_BACK, GL_AMBIENT_AND_DIFFUSE)  # 设置材质颜色
+        glEnable(GL_COLOR_MATERIAL)
+        
+        glutDisplayFunc(self.draw)  # 注册回调函数draw()
+        glutIdleFunc(self.draw)
+        glutReshapeFunc(reshape)  # 注册响应窗口改变的函数reshape()
+        # glutMouseFunc(mouseclick)  # 注册响应鼠标点击的函数mouseclick()
+        # glutMotionFunc(mousemotion)  # 注册响应鼠标拖拽的函数mousemotion()
+        # glutKeyboardFunc(keydown)  # 注册键盘输入的函数keydown()
+
+    def draw(self):
+        self.initRender()
+        glEnable(GL_LIGHTING)  # 启动光照
+        self.loop()
+        glDisable(GL_LIGHTING)  # 每次渲染后复位光照状态
+
+        # 把数据刷新到显存上
+        glFlush()
+        glutSwapBuffers()  # 切换缓冲区，以显示绘制内容
+
+    def initRender(self):
+        # 清除屏幕及深度缓存
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+
+        # 设置投影（透视投影）
+        glMatrixMode(GL_PROJECTION)
+        glLoadIdentity()
+
+        glFrustum(
+            VIEW[0], VIEW[1], VIEW[2] * WIN_H / WIN_W,
+            VIEW[3] * WIN_H / WIN_W, VIEW[4], VIEW[5], 
+        )
+
+        # 设置模型视图
+        glMatrixMode(GL_MODELVIEW)
+        glLoadIdentity()
+
+        # 几何变换
+        glScale(* SCALE_K)
+
+        # 设置视点
+        gluLookAt(
+            *self.eye,
+            *self.look_at, 
+            *EYE_UP, 
+        )
+
+        # 设置视口
+        glViewport(0, 0, WIN_W, WIN_H)
+
+def screenShot():
+    glReadBuffer(GL_FRONT)
+    # 从缓冲区中的读出的数据是字节数组
+    data = glReadPixels(0, 0, WIN_W, WIN_H, GL_RGB, GL_UNSIGNED_BYTE)
+    arr = np.zeros((WIN_W * WIN_H * 3), dtype=np.uint8)
+    for i in range(0, len(data), 3):
+        # 由于opencv中使用的是BGR而opengl使用的是RGB所以arr[i] = data[i+2]，而不是arr[i] = data[i]
+        arr[i] = data[i + 2]
+        arr[i + 1] = data[i + 1]
+        arr[i + 2] = data[i]
+    arr = np.reshape(arr, (WIN_H, WIN_W, 3))
+    # 因为opengl和OpenCV在Y轴上是颠倒的，所以要进行垂直翻转，可以查看cv2.flip函数
+    cv2.flip(arr, 0, arr)
+    resized = cv2.resize(arr, (RESOLUTION, RESOLUTION), interpolation=cv2.INTER_AREA)
+    return Image.fromarray(resized)
+
+def reshape(width, height):
+    global WIN_W, WIN_H
+    WIN_W, WIN_H = width, height
+    glutPostRedisplay()
--- main/render_dataset_two_body.py
+++ main/render_dataset_two_body.py
@@ -1,17 +1,13 @@
 from os import path
-import time
-from typing import List
-import json
 
 import numpy as np
 from OpenGL.GL import *
 from OpenGL.GLU import *
 from OpenGL.GLUT import *
-import cv2
-from PIL import Image
 
 from shared import *
 from physics_shared import *
+from render_dataset_shared import *
 from physics_two_body import *
 
 PATH = path.join(
@@ -23,220 +19,37 @@ PATH = path.join(
 CENTER_OF_MASS_STATIONARY = True
 REJECTABLE_START = np.inf
 
-WIN_W = 320
-WIN_H = 320
 SPF = .1
 DT = 1.5
-DRAW_GIRD = False
-
-MODE_LOCATE = 'locate'
-MODE_OBV_ONLY = 'obv_only'
-MODE_MAKE_IMG = 'make_img'
 
 # RUNNING_MODE = MODE_LOCATE
 RUNNING_MODE = MODE_OBV_ONLY
 # RUNNING_MODE = MODE_MAKE_IMG
 
-VIEW = np.array([-0.8, 0.8, -0.8, 0.8, 1.0, 100.0])  # 视景体的left/right/bottom/top/near/far六个面
-SCALE_K = np.array([1.0, 1.0, 1.0])  # 模型缩放比例
 EYE = np.array([0.0, -11.5, 0.0])  # 眼睛的位置（默认z轴的正方向）
 LOOK_AT = np.array([0.0, 0.0, 0.0])  # 瞄准方向的参考点（默认在坐标原点）
-EYE_UP = np.array([0.0, 0.0, 1.0])  # 定义对观察者而言的上方（默认y轴的正方向）
 
-class BallViewer:
-    def __init__(self) -> None:
-        self.trajectory: List[List[Body]] = None
-        self.stage: int = None
-        self.output_i = 0
-        self.render_or_screenshot = 0
-        self.frames: List[Image.Image] = None
-        self.reset()
-    
-    def reset(self):
-        self.trajectory, _ = oneLegalRun(
+class TwoBodyViewer(BallViewer):
+    def getTrajectory(self):
+        trajectory, _ = oneLegalRun(
             DT, SEQ_LEN, 
             CENTER_OF_MASS_STATIONARY, REJECTABLE_START, 
             EYE, 
         )
-        self.stage = 0
-        self.frames = []
+        return trajectory
 
-    def loop(self):
-        if self.render_or_screenshot == 0:
-            if RUNNING_MODE is MODE_LOCATE:
-                locate_with_ball()
-                return
-            
-            if self.stage >= SEQ_LEN:
-                if RUNNING_MODE is MODE_MAKE_IMG:
-                    self.saveVideo()
-                self.reset()
-                self.output_i += 1
-            
-            bodies = self.trajectory[self.stage]
-            self.makeBodies(*bodies)
-            
-            # step
-            self.stage += 1
-            if RUNNING_MODE is MODE_OBV_ONLY:
-                time.sleep(SPF)
-        else:
-            bodies = self.trajectory[self.stage - 1]
-            self.makeBodies(*bodies)
-            if RUNNING_MODE is MODE_MAKE_IMG:
-                self.frames.append(screenShot())
-        
-        self.render_or_screenshot = 1 - self.render_or_screenshot
-    
-    def makeBodies(self, a: Body, b: Body, /):
-        for body, color in (
-            (a, (0, 1, 0)), 
-            (b, (1, 0, 1)), 
-        ):
-            makeBall(*body.position, BALL_RADIUS, color)
-    
-    def saveVideo(self):
-        # self.frames[0].save(
-        #     f'{self.output_i}.gif', save_all=True, 
-        #     append_images=self.frames[1:], 
-        #     duration=GIF_INTERVAL, loop=0, 
-        # )
-        # GIF image compression is uncontrollable
-        os.makedirs(str(self.output_i), exist_ok=True)
-        os.chdir(str(self.output_i))
-        with open(TRAJ_FILENAME, 'w') as f:
-            trajectory_json = []
-            for bodies in self.trajectory:
-                bodies_json = []
-                trajectory_json.append(bodies_json)
-                for body in bodies:
-                    bodies_json.append(body.toJSON())
-            json.dump(trajectory_json, f)
-        for i, img in enumerate(self.frames):
-            img.save(f'{i}.png')
-        os.chdir('..')
+    def locate_with_ball(self):
+        radius = VIEW_RADIUS
+        center = (0, 0, 0)
 
-def makeBall(x, y, z, r=1, color3f=(0, 1, 0)):
-    glPushMatrix()
-    glColor3f(* color3f)
-    glTranslatef(x, y, z)
-    quad = gluNewQuadric()
-    gluSphere(quad, r, 90, 90)
-    gluDeleteQuadric(quad)
-    glPopMatrix()
-
-def locate_with_ball():
-    radius = VIEW_RADIUS
-    center = (0, 0, 0)
-
-    makeBall(*center, r=radius)
-
-def drawGrid():
-    glLineWidth(3)
-    glBegin(GL_LINES)
-    glColor4f(0.0, 0.0, 0.0, 1)  # 设置当前颜色为黑色不透明
-    for i in np.linspace(-15, 15, 19):
-        glVertex3f(i, -100, 0)
-        glVertex3f(i, +100, 0)
-        glVertex3f(-100, i, 0)
-        glVertex3f(+100, i, 0)
-    glEnd()
-
-def init():
-    glutInit()
-    displayMode = GLUT_DOUBLE | GLUT_ALPHA | GLUT_DEPTH
-    glutInitDisplayMode(displayMode)
-
-    glutInitWindowSize(WIN_W, WIN_H)
-    glutInitWindowPosition(300, 50)
-    glutCreateWindow('Ball Throwing Simulation')
-
-    # 初始化画布
-    glClearColor(0.4, 0.4, 0.4, 1.0)  # 设置画布背景色。注意：这里必须是4个参数
-    glEnable(GL_DEPTH_TEST)  # 开启深度测试，实现遮挡关系
-    glDepthFunc(GL_LEQUAL)  # 设置深度测试函数（GL_LEQUAL只是选项之一）-
-    glEnable(GL_LIGHT0)  # 启用0号光源
-    glLightfv(GL_LIGHT0, GL_POSITION, GLfloat_4(0, 1, 4, 0))  # 设置光源的位置
-    glLightfv(GL_LIGHT0, GL_SPOT_DIRECTION, GLfloat_3(0, 0, -1))  # 设置光源的照射方向
-    glColorMaterial(GL_FRONT_AND_BACK, GL_AMBIENT_AND_DIFFUSE)  # 设置材质颜色
-    glEnable(GL_COLOR_MATERIAL)
-    
-    glutDisplayFunc(draw)  # 注册回调函数draw()
-    glutIdleFunc(draw)
-    glutReshapeFunc(reshape)  # 注册响应窗口改变的函数reshape()
-    # glutMouseFunc(mouseclick)  # 注册响应鼠标点击的函数mouseclick()
-    # glutMotionFunc(mousemotion)  # 注册响应鼠标拖拽的函数mousemotion()
-    # glutKeyboardFunc(keydown)  # 注册键盘输入的函数keydown()
-
-def draw():
-    initRender()
-    glEnable(GL_LIGHTING)  # 启动光照
-    if DRAW_GIRD:
-        drawGrid()
-    ballViewer.loop()
-    glDisable(GL_LIGHTING)  # 每次渲染后复位光照状态
-
-    # 把数据刷新到显存上
-    glFlush()
-    glutSwapBuffers()  # 切换缓冲区，以显示绘制内容
-
-def initRender():
-    # 清除屏幕及深度缓存
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
-
-    # 设置投影（透视投影）
-    glMatrixMode(GL_PROJECTION)
-    glLoadIdentity()
-
-    glFrustum(
-        VIEW[0], VIEW[1], VIEW[2] * WIN_H / WIN_W,
-        VIEW[3] * WIN_H / WIN_W, VIEW[4], VIEW[5], 
-    )
-
-    # 设置模型视图
-    glMatrixMode(GL_MODELVIEW)
-    glLoadIdentity()
-
-    # 几何变换
-    glScale(* SCALE_K)
-
-    # 设置视点
-    gluLookAt(
-        * EYE,
-        * LOOK_AT, 
-        * EYE_UP, 
-    )
-
-    # 设置视口
-    glViewport(0, 0, WIN_W, WIN_H)
-
-def screenShot():
-    glReadBuffer(GL_FRONT)
-    # 从缓冲区中的读出的数据是字节数组
-    data = glReadPixels(0, 0, WIN_W, WIN_H, GL_RGB, GL_UNSIGNED_BYTE)
-    arr = np.zeros((WIN_W * WIN_H * 3), dtype=np.uint8)
-    for i in range(0, len(data), 3):
-        # 由于opencv中使用的是BGR而opengl使用的是RGB所以arr[i] = data[i+2]，而不是arr[i] = data[i]
-        arr[i] = data[i + 2]
-        arr[i + 1] = data[i + 1]
-        arr[i + 2] = data[i]
-    arr = np.reshape(arr, (WIN_H, WIN_W, 3))
-    # 因为opengl和OpenCV在Y轴上是颠倒的，所以要进行垂直翻转，可以查看cv2.flip函数
-    cv2.flip(arr, 0, arr)
-    resized = cv2.resize(arr, (RESOLUTION, RESOLUTION), interpolation=cv2.INTER_AREA)
-    return Image.fromarray(resized)
-
-def reshape(width, height):
-    global WIN_W, WIN_H
-    WIN_W, WIN_H = width, height
-    glutPostRedisplay()
+        self.makeBall(*center, r=radius)
 
 def main():
-    global ballViewer
     os.makedirs(PATH, exist_ok=True)
     os.chdir(PATH)
-    init()
-    ballViewer = BallViewer()
+    viewer = TwoBodyViewer(
+        RUNNING_MODE, BALL_RADIUS, EYE, LOOK_AT, SPF, 
+    )
     glutMainLoop()  # 进入glut主循环
 
 if __name__ == "__main__":

ce276f50ef4d32292b4c06ceec8d955e9fff4b05
Daniel Chin <daniel.chin@nyu.edu>
2022-10-24 14:31:46 +0400

    cleaner exp definition using .copy

--- main/template_two_body.py
+++ main/template_two_body.py
@@ -25,8 +25,8 @@ class MyExpGroup(ExperimentGroup):
 
 GROUPS = []
 
-hP = HyperParams()
-hP.lossWeightTree = LossWeightTree('total', 1, [
+template = HyperParams()
+template.lossWeightTree = LossWeightTree('total', 1, [
     LossWeightTree('self_recon', 1, None), 
     LossWeightTree('kld', 1e-5, None), 
     LossWeightTree('predict', 1, [
@@ -42,29 +42,36 @@ hP.lossWeightTree = LossWeightTree('total', 1, [
     ]), 
     LossWeightTree('symm_self_consistency', 0, None), 
 ])
-hP.lr = 0.001
-hP.symm = SymmetryAssumption(
+template.lr = 0.001
+template.symm = SymmetryAssumption(
     6, [
         ([Translate(3, 1), Rotate(3)], {Slice(0, 3), Slice(3, 6)}), 
     ], 
 )
-hP.supervise_rnn = False
-hP.supervise_vae = False
-hP.variational_rnn = True
-hP.vvrnn = False
-hP.vvrnn_static = -25
-hP.rnn_min_context = 4
-hP.rnn_width = 16
-hP.residual = True
-hP.jepa_stop_grad_encoder = True
-hP.vae_channels = [16, 32, 64]
-hP.deep_spread = False
-hP.batch_size = 256
-hP.grad_clip = .03
-hP.optim_name = 'adam'
-hP.train_set_size = 256
-hP.image_loss = 'mse'
-hP.teacher_forcing_duration = 40000
-hP.max_epoch = hP.teacher_forcing_duration
-hP.ready()
-GROUPS.append(MyExpGroup(hP))
+template.supervise_rnn = False
+template.supervise_vae = False
+template.variational_rnn = True
+template.vvrnn = False
+template.vvrnn_static = -25
+template.rnn_min_context = 4
+template.rnn_width = 16
+template.residual = True
+template.jepa_stop_grad_encoder = True
+template.vae_channels = [16, 32, 64]
+template.deep_spread = False
+template.batch_size = 256
+template.grad_clip = .03
+template.optim_name = 'adam'
+template.train_set_size = 256
+template.image_loss = 'mse'
+template.teacher_forcing_duration = 40000
+template.max_epoch = template.teacher_forcing_duration
+template.ready()
+GROUPS.append(MyExpGroup(template))
+
+# hP = template.copy()
+# hP.xxx = xxx
+# hP.ready()
+# GROUPS.append(MyExpGroup(hP))
+
+assert len(GROUPS) == 0

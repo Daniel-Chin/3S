4b810c3af2dc83c15d1599148fb5f4a68a891d3e
Daniel Chin <daniel.chin@nyu.edu>
2022-10-28 14:25:37 +0400

    evalDecoder: matrix

--- main/evalDecoder.py
+++ main/evalDecoder.py
@@ -15,56 +15,118 @@ from PIL.Image import Resampling
 
 from shared import torch2PIL
 from vae import VAE
+from template_bounce import MyExpGroup
 
 EXPERIMENT_PATH = path.join('./experiments', '''
-2022_m10_d27@13_42_53_rnn_width
+2022_m10_d27@13_22_30_z_loss_nostop
 '''.strip())
 LOCK_EPOCH = None
 
 RADIUS = 2
 TICK_INTERVAL = .5
+IMG_SIZE = 500
 
-class TestUI:
-    def __init__(self, vae, group_name, latent_dim):
-        self.vae: VAE = vae
-        self.latent_dim = latent_dim
+class UI:
+    def __init__(
+        self, groups: List[MyExpGroup], n_rand_inits, 
+        experiment_path, lock_epoch, 
+    ):
         self.win = tk.Tk()
-        self.win.title(group_name)
-        self.z = torch.zeros(
-            (latent_dim, ), dtype=torch.float, device=DEVICE, 
-        )
-        self.sliders: List[tk.Scale] = []
-        self.photo = None
-        self.label = tk.Label(self.win)
-        self.label.pack(side=tk.RIGHT)
+        self.win.title('Eval decoder')
 
+        self.groups = groups
+        self.n_row = n_rand_inits
+        self.n_col = len(groups)
+        self.vaes = [
+            [None] * self.n_col for _ in range(self.n_row)
+        ]
+        self.photoLabels: List[List[tk.Label]] = [
+            [None] * self.n_col for _ in range(self.n_row)
+        ]
+        self.photos = [
+            [None] * self.n_col for _ in range(self.n_row)
+        ]
+        self.max_latent_dim = 0
+        variable_names = set()
+        for col_i, group in enumerate(groups):
+            self.max_latent_dim = max(
+                self.max_latent_dim, 
+                group.hyperParams.symm.latent_dim, 
+            )
+            variable_names.add(group.variable_name)
+            groupLabel = tk.Label(
+                self.win, text=group.variable_value, 
+            )
+            groupLabel.grid(
+                row = 1, 
+                column = self.max_latent_dim + col_i, 
+                # padx=5, pady=5, 
+            )
+            for row_i in range(self.n_row):
+                vae = loadLatestModels(
+                    experiment_path, group, row_i, dict(
+                        vae=VAE, 
+                    ), lock_epoch, 
+                )['vae']
+                vae.eval()
+                self.vaes[row_i][col_i] = vae
+
+                label = tk.Label(self.win)
+                label.grid(
+                    row = row_i + 2, 
+                    column = self.max_latent_dim + col_i, 
+                    padx=5, pady=5, 
+                )
+                self.photoLabels[row_i][col_i] = label
+
+        self.z = torch.zeros((
+            self.max_latent_dim, 
+        ), dtype=torch.float, device=DEVICE)
+        topLabel = tk.Label(self.win, text=variable_names.pop())
+        assert not variable_names
+        topLabel.grid(
+            row=0, 
+            column=self.max_latent_dim, 
+            columnspan=self.n_col, 
+            # padx=5, pady=5, 
+        )
+
+        self.sliders: List[tk.Scale] = []
         self.initSliders()
 
     def initSliders(self):
-        for i in range(self.latent_dim):
+        for i in range(self.max_latent_dim):
             slider = tk.Scale(
                 self.win,
                 variable=tk.DoubleVar(value=self.z[i]),
                 command=lambda value, index=i : (
-                    self.onSliderUpdate(value, index)
+                    self.onSliderUpdate(index, value)
                 ),
                 from_=+ RADIUS, to=- RADIUS,
                 resolution=0.01, 
                 tickinterval=TICK_INTERVAL if i == 0 else 0, 
                 length=2000, width=100, sliderlength=100,
             )
-            slider.pack(side=tk.LEFT, padx=10)
+            slider.grid(
+                row=0, rowspan=self.n_row + 2, column=i, 
+                padx=10, 
+            )
             self.sliders.append(slider)
 
-    def onSliderUpdate(self, value, index):
+    def onSliderUpdate(self, index, value):
         value = float(value)
         self.z[index] = value
         # print(self.z)
         self.sliders[index].set(value)
-        img = decode(self.vae, self.z)
-        img = img.resize((1500, 1500), resample=Resampling.NEAREST)
-        self.photo = ImageTk.PhotoImage(img)
-        self.label.config(image=self.photo)
+        for col_i, vae_col in enumerate(self.vaes):
+            for row_i, vae in enumerate(vae_col):
+                img = decode(vae, self.z)
+                img = img.resize((
+                    IMG_SIZE, IMG_SIZE, 
+                ), resample=Resampling.NEAREST)
+                photo = ImageTk.PhotoImage(img)
+                self.photos[row_i][col_i] = photo
+                self.photoLabels[row_i][col_i].config(image=photo)
 
 def decode(vae: VAE, z: torch.Tensor):
     recon = vae.decode(z.unsqueeze(0))
@@ -75,23 +137,8 @@ def main(experiment_path, lock_epoch):
         experiment_path, EXPERIMENT_PY_FILENAME, 
     ))
     print(f'{exp_name = }')
-    for group in groups:
-        print(group.name())
-        group.hyperParams.print(depth=1)
-        for rand_init_i in range(n_rand_inits):
-            print(f'{rand_init_i = }')
-            vae = loadLatestModels(experiment_path, group, rand_init_i, dict(
-                vae=VAE, 
-            ), lock_epoch)['vae']
-            vae.eval()
-            test_ui = TestUI(vae, group.name(), group.hyperParams.symm.latent_dim)
-            try:
-                test_ui.win.mainloop()
-            finally:
-                try:
-                    test_ui.win.destroy()
-                except tk.TclError:
-                    pass
+    ui = UI(groups, n_rand_inits, experiment_path, lock_epoch)
+    ui.win.mainloop()
 
 if __name__ == '__main__':
     main(EXPERIMENT_PATH, LOCK_EPOCH)

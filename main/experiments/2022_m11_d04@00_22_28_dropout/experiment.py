9a12dc752f9ab9a1667cbd308e92481145bf2583
Daniel Chin <daniel.chin@nyu.edu>
2022-11-05 12:35:40 +0800

    auto commit exp

---
+++ main/experiments/2022_m11_d04@00_22_28_dropout/experiment.py
@@ -0,0 +1,86 @@
+from functools import lru_cache
+from symmetry_transforms import *
+from torchWork import LossWeightTree, ExperimentGroup
+
+from shared import *
+
+TRAIN_SET_PATH    = '../datasets/bounce/train'
+VALIDATE_SET_PATH = '../datasets/bounce/validate'
+VALIDATE_SET_SIZE = 64
+ACTUAL_DIM = 3
+
+EXP_NAME = 'dropout'
+N_RAND_INITS = 4
+
+class MyExpGroup(ExperimentGroup):
+    def __init__(self, hyperParams: HyperParams) -> None:
+        self.hyperParams = hyperParams
+
+        self.variable_name = 'dropout'
+        self.variable_value = hyperParams.dropout
+    
+    @lru_cache(1)
+    def name(self):
+        return f'{self.variable_name}={self.variable_value}'
+
+GROUPS = []
+
+template = HyperParams()
+template.lossWeightTree = LossWeightTree('total', 1, [
+    LossWeightTree('self_recon', 1, None), 
+    LossWeightTree('kld', 1e-5, None), 
+    LossWeightTree('seq_energy', 0, [
+        LossWeightTree('real', 0, None), 
+        LossWeightTree('fake', 0, None), 
+    ]), 
+    LossWeightTree('predict', 1, [
+        LossWeightTree('z', 0, None), 
+        LossWeightTree('image', 1, None), 
+    ]), 
+    LossWeightTree('supervise', 0, [
+        LossWeightTree('rnn', 0, None), 
+        LossWeightTree('vae', 0, [
+            LossWeightTree('encode', 0, None), 
+            LossWeightTree('decode', 0, None), 
+        ]), 
+    ]), 
+    LossWeightTree('symm_self_consistency', 0, None), 
+])
+template.lr = 0.001
+template.symm = SymmetryAssumption(
+    3, [
+        ([Translate(2, 1), Rotate(2)], {Slice(0, 2)}), 
+        ([Trivial()], {Slice(2, 3)}), 
+    ], 
+)
+template.supervise_rnn = False
+template.supervise_vae = False
+template.supervise_vae_only_xy = False
+template.variational_rnn = True
+template.vvrnn = False
+template.vvrnn_static = -25
+template.rnn_min_context = 4
+template.energy_noise_std = 1
+template.rnn_width = 16
+template.residual = True
+template.jepa_stop_grad_encoder = False
+template.dropout = None
+template.vae_channels = [16, 32, 64]
+template.deep_spread = False
+template.batch_size = 128
+template.grad_clip = 1
+template.optim_name = 'adam'
+template.train_set_size = 128
+template.image_loss = 'mse'
+template.teacher_forcing_duration = 40000
+template.max_epoch = template.teacher_forcing_duration
+template.ready()
+
+# modifying template
+# template.xxx = xxx
+
+for d in (0, .1, .5):
+    hP = template.copy()
+    hP.dropout = d
+    hP.ready()
+    GROUPS.append(MyExpGroup(hP))

8eb5c157da86260f80a459bf7e21cb22045f71ee
Daniel Chin <daniel.chin@nyu.edu>
2022-11-28 11:20:24 +0400

    mse loss now doesn't assume Y to come with 0 mean

--- main/linearity_metric.py
+++ main/linearity_metric.py
@@ -1,26 +1,21 @@
 import torch
 from torch.linalg import lstsq
+import torch.nn.functional as F
 from shared import *
 
 def projectionMSE(X: torch.Tensor, Y: torch.Tensor):
-    # A better method is to pad 1. I didn't know that! 
+    Y = Y - Y.mean(dim=0)
     Y = Y / Y.std(dim=0)
 
-    X_mean = X.mean(dim=0)
-    X_centered = X - X_mean 
-    Y_mean = Y.mean(dim=0)
-    Y_centered = Y - Y_mean 
+    X = F.pad(X, (1, 0), value=1)
 
-    solution_centered, residuals, _, _ = lstsq(X_centered, Y_centered)
-    solution_centered: torch.Tensor
+    solution, residuals, _, _ = lstsq(X, Y)
+    solution: torch.Tensor
 
     # Some versions of pyTorch has bugs with `residuals`. 
     # Let's not use it. It doesn't save much time anyways. 
     
-    coef = solution_centered
-    intercept = Y_mean - X_mean @ solution_centered
-
-    Y_hat = X @ coef + intercept
+    Y_hat = X @ solution
 
     return (Y - Y_hat).square().mean().cpu()
 

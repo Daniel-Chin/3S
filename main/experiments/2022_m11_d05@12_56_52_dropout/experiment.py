6a54ea7960274e9276fc8bc3d0867f12900ef1da
Daniel Chin <daniel.chin@nyu.edu>
2022-11-28 11:15:27 +0400

    modify archive

--- main/experiments/2022_m11_d05@12_56_52_dropout/experiment.py
+++ main/experiments/2022_m11_d05@12_56_52_dropout/experiment.py
@@ -8,6 +8,7 @@ TRAIN_SET_PATH    = '../datasets/bounce/train'
 VALIDATE_SET_PATH = '../datasets/bounce/validate'
 VALIDATE_SET_SIZE = 64
 ACTUAL_DIM = 3
+SEQ_LEN = 20
 
 EXP_NAME = 'dropout'
 N_RAND_INITS = 4

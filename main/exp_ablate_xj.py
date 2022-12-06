d48bd1023de8774344e213a9a4372f1100fd2dcc
Daniel Chin <daniel.chin@nyu.edu>
2022-12-06 13:25:25 +0400

    conclude

--- main/exp_ablate_xj.py
+++ main/exp_ablate_xj.py
@@ -80,60 +80,74 @@ template.sched_sampling = LinearScheduledSampling(40000)
 template.max_epoch = template.sched_sampling.duration
 template.ready()
 
+xJ = template.copy()
+xJ.xj_new_dan = 0
 hP = template.copy()
 hP.xj_new_dan = 1
-hP.vae_channels = [64, 128, 256]
-hP.deep_spread = True
-hP.relu_leak = False
-hP.lossWeightTree['self_recon'].weight = (
-    RESOLUTION ** 2 * SEQ_LEN * hP.batch_size
-)
-hP.lossWeightTree['kld'].weight = 0.01 * hP.batch_size
-hP.lossWeightTree['predict']['z'].weight = (
-    2 * hP.symm.latent_dim * SEQ_LEN * hP.batch_size
-)
-hP.lossWeightTree['predict']['image'].weight = (
-    2 * RESOLUTION ** 2 * SEQ_LEN * hP.batch_size
-)
-hP.K = 2    # one for translate, one for rotate
-hP.symm = GusMethod()
-hP.grad_clip = None
-hP.rnn_min_context = 5
-hP.sched_sampling = SigmoidScheduledSampling(alpha=2200, beta=8000)
-hP.max_epoch = 150001 // hP.batch_size
-hP.ready()
-GROUPS.append(MyExpGroup(hP))
 
-hP = template.copy()
-hP.xj_new_dan = 0
-hP.rnn_width = 256
+xJ.rnn_width = 256
+
+xJ.vae_channels = [64, 128, 256]
 hP.vae_channels = [64, 128, 256]
+
+xJ.deep_spread = True
 hP.deep_spread = True
+
+xJ.relu_leak = False
 hP.relu_leak = False
-hP.batch_size = 32
-hP.lossWeightTree['self_recon'].weight = (
-    RESOLUTION ** 2 * SEQ_LEN * hP.batch_size
+
+xJ.batch_size = 32
+
+xJ.lossWeightTree['self_recon'].weight = (
+    RESOLUTION ** 2 * SEQ_LEN * xJ.batch_size
 )
-hP.lossWeightTree['kld'].weight = 0.01 * hP.batch_size
+hP.lossWeightTree['self_recon'].weight = (
+    RESOLUTION ** 2 * SEQ_LEN * xJ.batch_size
+)
+xJ.lossWeightTree['kld'].weight = 0.01 * xJ.batch_size
+hP.lossWeightTree['kld'].weight = 0.01 * xJ.batch_size
+xJ.lossWeightTree['predict']['z'].weight = (
+    2 * xJ.symm.latent_dim * SEQ_LEN * xJ.batch_size
+)
 hP.lossWeightTree['predict']['z'].weight = (
-    2 * hP.symm.latent_dim * SEQ_LEN * hP.batch_size
+    2 * hP.symm.latent_dim * SEQ_LEN * xJ.batch_size
+)
+xJ.lossWeightTree['predict']['image'].weight = (
+    2 * RESOLUTION ** 2 * SEQ_LEN * xJ.batch_size
 )
 hP.lossWeightTree['predict']['image'].weight = (
-    2 * RESOLUTION ** 2 * SEQ_LEN * hP.batch_size
+    2 * RESOLUTION ** 2 * SEQ_LEN * xJ.batch_size
 )
+
+xJ.K = 2    # one for translate, one for rotate
 hP.K = 2    # one for translate, one for rotate
+
+xJ.symm = GusMethod()
 hP.symm = GusMethod()
+
 def f(epoch, batch_i, hParams: HyperParams):
     return 0.99999 ** (epoch * hParams.n_batches_per_epoch + batch_i)
-hP.lr_diminish = f
+xJ.lr_diminish = f
+
+xJ.grad_clip = None
 hP.grad_clip = None
+
+xJ.rnn_min_context = 5
 hP.rnn_min_context = 5
+
+xJ.sched_sampling = SigmoidScheduledSampling(alpha=2200, beta=8000)
 hP.sched_sampling = SigmoidScheduledSampling(alpha=2200, beta=8000)
-hP.max_epoch = 150001 // hP.batch_size
-hP.residual = False
-hP.image_loss = 'bce'
+
+xJ.max_epoch = 150001 // xJ.batch_size
+hP.max_epoch = 150001 // xJ.batch_size
+
+xJ.residual = False
+
+xJ.image_loss = 'bce'
+
 hP.ready()
-xj = hP
-GROUPS.append(MyExpGroup(xj))
+GROUPS.append(MyExpGroup(hP))
+xJ.ready()
+GROUPS.append(MyExpGroup(xJ))
 
 assert len(GROUPS) == 2

ea1302e398ef883aec831e5fe255fdd17e574fee
Daniel Chin <nq285@log-1.hpc.nyu.edu>
2022-12-18 13:10:09 -0500

    auto commit exp

---
+++ main/experiments/2022_m12_d18@01_30_52_simsiam/experiment.py
@@ -0,0 +1,92 @@
+from functools import lru_cache
+from symmetry_transforms import *
+from torchWork import LossWeightTree, ExperimentGroup
+import numpy as np
+
+from shared import *
+
+TRAIN_SET_PATH    = '../datasets/bounce/train'
+VALIDATE_SET_PATH = '../datasets/bounce/validate'
+VALIDATE_SET_SIZE = 64
+SEQ_LEN = 20
+ACTUAL_DIM = 3
+
+EXP_NAME = 'simsiam'
+N_RAND_INITS = 1
+
+class MyExpGroup(ExperimentGroup):
+    def __init__(self, hyperParams: HyperParams) -> None:
+        self.hyperParams = hyperParams
+
+        self.variable_name = 'SimSiam,z_loss_weight'
+        self.variable_value = (
+            hyperParams.jepa_stop_grad_r_encoder, 
+            hyperParams.lossWeightTree['predict']['z'].weight, 
+        )
+    
+    @lru_cache(1)
+    def name(self):
+        return f'{self.variable_name}={self.variable_value}'
+
+GROUPS = []
+
+template = HyperParams()
+template.lossWeightTree = LossWeightTree('total', 1, [
+    LossWeightTree('self_recon', 1.31072, None), 
+    LossWeightTree('kld', 3.2e-7, None), 
+    LossWeightTree('seq_energy', 0, [
+        LossWeightTree('real', 0, None), 
+        LossWeightTree('fake', 0, None), 
+    ]), 
+    LossWeightTree('predict', 1, [
+        LossWeightTree('z', None, None), 
+        LossWeightTree('image', 2.62144, None), 
+    ]), 
+    LossWeightTree('supervise', 0, [
+        LossWeightTree('rnn', 0, None), 
+        LossWeightTree('vae', 0, [
+            LossWeightTree('encode', 0, None), 
+            LossWeightTree('decode', 0, None), 
+        ]), 
+    ]), 
+    LossWeightTree('symm_self_consistency', 0, None), 
+])
+template.lr = 0.001
+template.symm = GusMethod()
+template.supervise_rnn = False
+template.supervise_vae = False
+template.supervise_vae_only_xy = False
+template.variational_rnn = True
+template.vvrnn = False
+template.vvrnn_static = -25
+template.rnn_min_context = 5
+template.energy_noise_std = 1
+template.rnn_width = 32
+template.residual = False
+template.jepa_stop_grad_l_encoder = False
+template.jepa_stop_grad_r_encoder = False
+template.dropout = 0.0
+template.vae_channels = [64, 128, 256]
+template.deep_spread = True
+template.relu_leak = False
+template.vae_kernel_size = 4
+template.batch_size = 32
+template.grad_clip = None
+template.optim_name = 'adam'
+template.lr_diminish = None
+template.train_set_size = 128
+template.image_loss = 'mse'
+template.sched_sampling = LinearScheduledSampling(4000)
+template.max_epoch = template.sched_sampling.duration
+template.ready()
+
+# modifying template
+# template.xxx = xxx
+
+for do_stop in (False, True):
+    for w in np.exp(np.linspace(np.log(3.84e-3), np.log(1.3e-2), 12)):
+        hP = template.copy()
+        hP.lossWeightTree['predict']['z'].weight = w
+        hP.jepa_stop_grad_r_encoder = do_stop
+        hP.ready()
+        GROUPS.append(MyExpGroup(hP))

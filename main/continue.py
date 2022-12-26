610adc1f8e8fc884dbda4f301156de1cbbf1d288
Daniel Chin <daniel.chin@nyu.edu>
2022-12-26 12:14:36 +0400

    conitnue

--- main/continue.py
+++ main/continue.py
@@ -13,7 +13,7 @@ from one_epoch import oneEpoch
 from vae import VAE
 from rnn import PredRNN, EnergyRNN
 
-EXP_DIR = '2022_m12_d20@01_35_36_over_param_vae'
+EXP_DIR = '2022_m12_d19@07_44_07_train_size_c_max_batch'
 
 def continueTrain():
     exp_py_path = path.join(

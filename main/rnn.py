c7380456542f7fdbded865c6c964e0181322f90f
Daniel Chin <daniel.chin@nyu.edu>
2023-01-04 11:49:17 +0400

    first

--- main/rnn.py
+++ main/rnn.py
@@ -7,16 +7,21 @@ class PredRNN(nn.Module):
         super().__init__()
         self.hParams = hyperParams
         hidden_dim = hyperParams.rnn_width
+        ensemble = hyperParams.rnn_ensemble
 
-        self.updateHidden = nn.Sequential(
+        self.updateHidden = [nn.Sequential(
             nn.Linear(hidden_dim + hyperParams.symm.latent_dim, hidden_dim), 
             nn.Tanh(), 
-        )
+        ) for _ in range(ensemble)]
         self.dropout = nn.Dropout(hyperParams.dropout)
         
-        self.projHead = nn.Linear(hidden_dim, hyperParams.symm.latent_dim)
+        self.projHead = [nn.Linear(
+            hidden_dim, hyperParams.symm.latent_dim, 
+        ) for _ in range(ensemble)]
         if hyperParams.vvrnn:
-            self.logVarHead = nn.Linear(hidden_dim, hyperParams.symm.latent_dim)
+            self.logVarHead = [nn.Linear(
+                hidden_dim, hyperParams.symm.latent_dim, 
+            ) for _ in range(ensemble)]
 
         self.hidden = None
         
@@ -26,21 +31,25 @@ class PredRNN(nn.Module):
         ), flush=True)
     
     def zeroHidden(self, batch_size, device):
-        self.hidden = torch.zeros((
-            batch_size, self.hParams.rnn_width, 
-        ), device=device)
+        self.hidden = [
+            torch.zeros((
+                batch_size, self.hParams.rnn_width, 
+            ), device=device) 
+            for _ in range(self.hParams.rnn_ensemble)
+        ]
     
     def stepTime(self, z_t: torch.Tensor):
         '''
         `z` is (BATCH_SIZE, SEQ_LEN, latent_dim). 
         '''
-        output = self.updateHidden(torch.cat((
-            self.dropout(self.hidden), z_t, 
-        ), dim=1))
-        if self.hParams.residual:
-            self.hidden = self.hidden * self.hParams.residual + output
-        else:
-            self.hidden = output
+        for i in range(self.hParams.rnn_ensemble):
+            output = self.updateHidden[i](torch.cat((
+                self.dropout(self.hidden[i]), z_t, 
+            ), dim=1))
+            if self.hParams.residual:
+                self.hidden[i] = self.hidden[i] * self.hParams.residual + output
+            else:
+                self.hidden[i] = output
 
 class EnergyRNN(nn.Module):
     def __init__(self, hyperParams: HyperParams) -> None:

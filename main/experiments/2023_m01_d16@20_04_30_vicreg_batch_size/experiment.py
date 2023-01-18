fdb2b3621dba6aa83515b04be47183160a1f993d
Daniel Chin (NY HPC) <daniel.chin@nyu.edu>
2023-01-18 10:49:04 -0500

    auto commit exp

---
+++ main/experiments/2023_m01_d16@20_04_30_vicreg_batch_size/experiment.py
@@ -0,0 +1,135 @@
+from functools import lru_cache
+from symmetry_transforms import *
+from torchWork import LossWeightTree, ExperimentGroup
+
+from shared import *
+
+TRAIN_SET_PATH    = '../datasets/bounce/train'
+VALIDATE_SET_PATH = '../datasets/bounce/validate'
+VALIDATE_SET_SIZE = 64
+SEQ_LEN = 20
+ACTUAL_DIM = 3
+
+EXP_NAME = 'vicreg_batch_size'
+N_RAND_INITS = 4
+
+class MyExpGroup(ExperimentGroup):
+    def __init__(self, hyperParams: HyperParams) -> None:
+        self.hyperParams = hyperParams
+
+        self.variable_name = 'batch_size'
+        self.variable_value = hyperParams.batch_size
+    
+    @lru_cache(1)
+    def name(self):
+        return f'{self.variable_name}={self.variable_value}'
+
+GROUPS = []
+
+template = HyperParams()
+template.lossWeightTree = LossWeightTree('total', 1, [
+    LossWeightTree('self_recon', 1.31072, None), 
+    LossWeightTree('kld', 3.2e-7, None), 
+    LossWeightTree('seq_energy', 0, [
+        LossWeightTree('real', 0, None), 
+        LossWeightTree('fake', 0, None), 
+    ]), 
+    LossWeightTree('predict', 1, [
+        LossWeightTree('z', 3.84e-3, None), 
+        LossWeightTree('image', 2.62144, None), 
+    ]), 
+    LossWeightTree('supervise', 0, [
+        LossWeightTree('rnn', 0, None), 
+        LossWeightTree('vae', 0, [
+            LossWeightTree('encode', 0, None), 
+            LossWeightTree('decode', 0, None), 
+        ]), 
+    ]), 
+    LossWeightTree('vicreg', 0, [
+        LossWeightTree('variance', 0, None), 
+        LossWeightTree('invariance', 0, None), 
+        LossWeightTree('covariance', 0, None), 
+    ]), 
+    LossWeightTree('symm_self_consistency', 0, None), 
+])
+template.lr = 0.001
+template.symm = SymmetryAssumption(
+    3, [
+        (SAMPLE_TRANS, [Translate(2, 1), Rotate(2)], {Slice(0, 2)}), 
+        (SAMPLE_TRANS, [Trivial()], {Slice(2, 3)}), 
+    ], 
+)
+template.supervise_rnn = False
+template.supervise_vae = False
+template.supervise_vae_only_xy = False
+template.variational_rnn = True
+template.vvrnn = False
+template.vvrnn_static = None
+template.rnn_min_context = 5
+template.energy_noise_std = 1
+template.rnn_width = 32
+template.residual = False
+template.jepa_stop_grad_l_encoder = False
+template.jepa_stop_grad_r_encoder = False
+template.dropout = 0.0
+template.rnn_ensemble = 1
+template.vae_channels = [64, 128, 256]
+template.deep_spread = True
+template.relu_leak = False
+template.vae_kernel_size = 4
+template.vae_is_actually_ae = False
+template.encoder_batch_norm = True
+template.batch_size = 16
+template.grad_clip = None
+template.optim_name = 'adam'
+template.weight_decay = 0
+template.lr_diminish = None
+template.train_set_size = 64
+template.sched_image_loss = ScheduledImageLoss((0, 'mse'))
+template.sched_sampling = LinearScheduledSampling(18000)
+template.max_epoch = template.sched_sampling.duration
+template.vicreg_expander_identity = None
+template.vicreg_expander_widths = None
+template.vicreg_invariance_on_Y = None
+
+# modifying template
+# template.xxx = xxx
+
+vicreg = template.copy()
+vicreg.lossWeightTree['vicreg'].weight = 1
+vicreg.lossWeightTree['vicreg']['variance'].weight = 25
+vicreg.lossWeightTree['vicreg']['invariance'].weight = 25
+vicreg.lossWeightTree['vicreg']['covariance'].weight = 1
+vicreg.vicreg_expander_identity = False
+vicreg.vicreg_expander_widths = [64, 64, 64]
+vicreg.vicreg_invariance_on_Y = False
+vicreg.weight_decay = 1e-9
+vicreg.batch_size = 32
+vicreg.lossWeightTree['self_recon'].weight = 0
+vicreg.lossWeightTree['kld'].weight = 0
+vicreg.lossWeightTree['predict'].weight = 0
+vicreg.lossWeightTree['predict']['image'].weight = 0
+vicreg.lossWeightTree['predict']['z'].weight = 0
+vicreg.vae_is_actually_ae = True
+vicreg.variational_rnn = False
+
+# modify vicreg from vanilla
+vicreg.vicreg_expander_identity = True
+vicreg.vicreg_expander_widths = None
+vicreg.train_set_size = 1024
+
+for bs in [
+    32, 64, 128, 256, 512, 1024, 
+]:
+    hP = vicreg.copy()
+    hP.batch_size = bs
+    hP.ready(globals())
+    GROUPS.append(MyExpGroup(hP))
+
+
+# hP = vicreg.copy()
+# hP.nickname = 'vi=100'
+# hP.lossWeightTree['vicreg']['variance'].weight = 100
+# hP.lossWeightTree['vicreg']['invariance'].weight = 100
+# hP.ready(globals())
+# GROUPS.append(MyExpGroup(hP))
